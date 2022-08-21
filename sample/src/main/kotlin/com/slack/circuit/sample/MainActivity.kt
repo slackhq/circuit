ed47aaaecea6ab26bfbf5fff12f32b76e2c6c0db
Zac Sweers <pandanomic@gmail.com>
2022-08-21 15:17:17 -0400

    Generalize VMFactory

--- sample/src/main/kotlin/com/slack/circuit/sample/MainActivity.kt
+++ sample/src/main/kotlin/com/slack/circuit/sample/MainActivity.kt
@@ -20,14 +20,14 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.slack.circuit.Navigator
-import com.slack.circuit.sample.di.CircuitViewModelProviderFactory
+import com.slack.circuit.sample.di.StarViewModelProviderFactory
 import com.slack.circuit.sample.petlist.PetListScreen
 import com.slack.circuit.sample.ui.StarTheme
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
-  @Inject lateinit var viewModelProviderFactory: CircuitViewModelProviderFactory
+  @Inject lateinit var viewModelProviderFactory: ViewModelProvider.Factory
   @Inject lateinit var navigatorFactory: Navigator.Factory<*>
 
   override fun onCreate(savedInstanceState: Bundle?) {

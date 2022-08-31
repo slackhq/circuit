3580fa4e8badd8ad7717c1ad1734b738b6ff316c
Kieran Elliott <kierse@pissiphany.com>
2022-08-31 16:35:28 -0400

    Replaced Animal with PetListAnimal

--- sample/src/androidTest/kotlin/com/slack/circuit/sample/petlist/PetListTest.kt
+++ sample/src/androidTest/kotlin/com/slack/circuit/sample/petlist/PetListTest.kt
@@ -22,11 +22,6 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import com.google.common.truth.Truth.assertThat
-import com.slack.circuit.sample.data.Animal
-import com.slack.circuit.sample.data.Breeds
-import com.slack.circuit.sample.data.Colors
-import com.slack.circuit.sample.data.Link
-import com.slack.circuit.sample.data.Links
 import com.slack.circuit.sample.petlist.PetListTestConstants.BREED_TAG
 import com.slack.circuit.sample.petlist.PetListTestConstants.GENDER_AND_AGE_TAG
 import com.slack.circuit.sample.petlist.PetListTestConstants.GRID_TAG
@@ -66,7 +61,7 @@ class PetListTest {
 
   @Test
   fun petList_show_list_for_success_state() {
-    val animals = listOf(animal.toPetListAnimal())
+    val animals = listOf(ANIMAL)
 
     composeTestRule.run {
       setContent { PetList(PetListScreen.State.Success(animals)) {} }
@@ -84,8 +79,7 @@ class PetListTest {
   @Test
   fun petList_emits_event_when_tapping_on_animal() = runTest {
     val channel = Channel<Any>(1)
-    val petListAnimal = animal.toPetListAnimal()
-    val animals = listOf(petListAnimal)
+    val animals = listOf(ANIMAL)
 
     composeTestRule.run {
       setContent { PetList(PetListScreen.State.Success(animals), channel::trySend) }
@@ -94,34 +88,18 @@ class PetListTest {
 
       val event = channel.receive()
       assertThat(event)
-        .isEqualTo(PetListScreen.Event.ClickAnimal(petListAnimal.id, petListAnimal.imageUrl))
+        .isEqualTo(PetListScreen.Event.ClickAnimal(ANIMAL.id, ANIMAL.imageUrl))
     }
   }
 
   private companion object {
-    val animal =
-      Animal(
-        id = 1L,
-        organizationId = "organizationId",
-        url = "url",
-        type = "type",
-        species = "species",
-        breeds = Breeds("Australian Terrier"),
-        colors = Colors(),
-        age = "12",
-        size = "size",
-        coat = "coat",
-        name = "Baxter",
-        description = "description",
-        gender = "male",
-        photos = emptyList(),
-        videos = emptyList(),
-        status = "status",
-        attributes = emptyMap(),
-        environment = emptyMap(),
-        tags = emptyList(),
-        publishedAt = "publishedAt",
-        links = Links(self = Link("self"), type = Link("type"), organization = Link("organization"))
-      )
+    val ANIMAL = PetListAnimal(
+      id = 1L,
+      name = "Baxter",
+      imageUrl = null,
+      breed = "Australian Terrier",
+      gender = "male",
+      age = "12"
+    )
   }
 }

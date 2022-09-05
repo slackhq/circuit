bdb0778b63a3fac9d4d2b2b15caa7cb50ece40a2
Zac Sweers <pandanomic@gmail.com>
2022-09-04 21:53:41 -0400

    Reconfigure circuit core to be KMP

--- circuit/build.gradle.kts
+++ circuit/build.gradle.kts
@@ -1,6 +1,8 @@
+import org.jetbrains.kotlin.gradle.plugin.PLUGIN_CLASSPATH_CONFIGURATION_NAME
+
 plugins {
   id("com.android.library")
-  kotlin("android")
+  kotlin("multiplatform")
 }
 
 if (hasProperty("SlackRepositoryUrl")) {
@@ -11,11 +13,46 @@ android {
   namespace = "com.slack.circuit.core"
 }
 
+kotlin {
+  //region KMP Targets
+  android {
+    publishLibraryVariants("release")
+  }
+  jvm()
+  //endregion
+
+  sourceSets {
+    commonMain {
+      dependencies {
+        api(libs.compose.runtime)
+        api(libs.coroutines)
+      }
+    }
+    maybeCreate("androidMain").apply {
+      dependencies {
+        api(projects.backstack)
+      }
+    }
+    maybeCreate("commonTest").apply {
+      dependencies {
+        implementation(libs.kotlin.test)
+      }
+    }
+    val commonJvmTest = maybeCreate("commonJvmTest").apply {
+      dependencies {
+        implementation(libs.junit)
+        implementation(libs.truth)
+      }
+    }
+    maybeCreate("androidTest").apply {
+      dependsOn(commonJvmTest)
+    }
+    maybeCreate("jvmTest").apply {
+      dependsOn(commonJvmTest)
+    }
+  }
+}
+
 dependencies {
-  api(libs.androidx.compose.integration.viewModel)
-  api(libs.androidx.compose.integration.activity)
-  api(libs.bundles.compose)
-  api(projects.backstack)
-  testImplementation(libs.junit)
-  testImplementation(libs.truth)
+  add(PLUGIN_CLASSPATH_CONFIGURATION_NAME, libs.androidx.compose.compiler)
 }

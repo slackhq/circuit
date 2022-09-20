945015cca43900b39d06a2ecf384bb5e9bce0afd
Jonnathan Petote <jpetote@jpetote-ltmpedq.internal.salesforce.com>
2022-09-20 14:32:58 -0400

    WIP: In a working state, need to figure out CircuitContent issue with NoOp navigator.

--- sample/src/main/kotlin/com/slack/circuit/sample/home/HomePresenter.kt
+++ sample/src/main/kotlin/com/slack/circuit/sample/home/HomePresenter.kt
@@ -16,22 +16,36 @@
 package com.slack.circuit.sample.home
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.IconButton
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FilterList
+import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
+import com.slack.circuit.CircuitContent
 import com.slack.circuit.Navigator
 import com.slack.circuit.Presenter
 import com.slack.circuit.PresenterFactory
@@ -41,7 +55,8 @@ import com.slack.circuit.UiFactory
 import com.slack.circuit.sample.di.AppScope
 import com.slack.circuit.sample.petlist.About
 import com.slack.circuit.sample.petlist.Gender
-import com.slack.circuit.sample.petlist.PetList
+import com.slack.circuit.sample.petlist.PetListFilterPresenter
+import com.slack.circuit.sample.petlist.PetListFilterScreen
 import com.slack.circuit.sample.petlist.PetListPresenter
 import com.slack.circuit.sample.petlist.PetListScreen
 import com.slack.circuit.sample.petlist.Size
@@ -50,22 +65,24 @@ import com.squareup.anvil.annotations.ContributesMultibinding
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
-import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.filterIsInstance
 import kotlinx.coroutines.flow.map
 import kotlinx.parcelize.Parcelize
+import javax.inject.Inject
 
 @Parcelize
 object HomeScreen : Screen {
   data class State(
     val homeNavState: HomeNavScreen.HomeNavState,
-    val petListState: PetListScreen.State
+    val petListState: PetListScreen.State,
+    val petListFilterState: PetListFilterScreen.State
   )
 
   sealed interface Event {
     class HomeEvent(val event: HomeNavScreen.Event.HomeNavEvent) : Event
     class PetListEvent(val event: PetListScreen.Event) : Event
+    class PetListFilterEvent(val event: PetListFilterScreen.Event) : Event
   }
 }
 
@@ -83,21 +100,27 @@ class HomePresenter
 @AssistedInject
 constructor(
   @Assisted private val navigator: Navigator,
-  petListPresenterFactory: PetListPresenter.Factory
+  petListPresenterFactory: PetListPresenter.Factory,
+  petListFilterPresenterFactory: PetListFilterPresenter.Factory
 ) : Presenter<HomeScreen.State, HomeScreen.Event> {
+  // Should this presenter should be set up with the states returned from Pet List Filter Presenter?
   private val petListPresenter = petListPresenterFactory.create(navigator, PetListScreen(Gender.ALL, Size.ALL))
+  private val petListFilterPresenter = petListFilterPresenterFactory.create()
 
   @SuppressLint("FlowOperatorInvokedInComposition")
   @Composable
   override fun present(events: Flow<HomeScreen.Event>): HomeScreen.State {
     val homeNavState =
       homeNavPresenter(events.filterIsInstance<HomeScreen.Event.HomeEvent>().map { it.event })
+    val petListFilterState = petListFilterPresenter.present(
+      events.filterIsInstance<HomeScreen.Event.PetListFilterEvent>().map { it.event }
+    )
     val petListState =
       petListPresenter.present(
         events.filterIsInstance<HomeScreen.Event.PetListEvent>().map { it.event }
       )
 
-    return HomeScreen.State(homeNavState, petListState)
+    return HomeScreen.State(homeNavState, petListState, petListFilterState)
   }
 
   @AssistedFactory
@@ -119,33 +142,81 @@ class HomeScreenFactory @Inject constructor() : UiFactory {
 private fun homeUi() =
   ui<HomeScreen.State, HomeScreen.Event> { state, events -> HomeContent(state, events) }
 
+@OptIn(ExperimentalMaterialApi::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun HomeContent(state: HomeScreen.State, eventSink: (HomeScreen.Event) -> Unit) {
-  Scaffold(
-    modifier = Modifier.navigationBarsPadding().systemBarsPadding().fillMaxWidth(),
-    topBar = {
-      CenterAlignedTopAppBar(
-        title = {
-          Text("Adoptables", fontSize = 22.sp, color = MaterialTheme.colorScheme.onPrimaryContainer)
-        },
-        colors =
-          TopAppBarDefaults.centerAlignedTopAppBarColors(
-            containerColor = MaterialTheme.colorScheme.primaryContainer
-          )
-      )
-    },
-    bottomBar = {
-      BottomNavigationBar(selectedIndex = state.homeNavState.index) { index ->
-        eventSink(HomeScreen.Event.HomeEvent(HomeNavScreen.Event.HomeNavEvent(index)))
+  val modifier = Modifier
+    .navigationBarsPadding()
+    .systemBarsPadding()
+    .fillMaxWidth()
+
+  val modalState =
+    rememberModalBottomSheetState(
+      initialValue =
+      if (state.petListFilterState.showBottomSheet) ModalBottomSheetValue.Expanded else ModalBottomSheetValue.Hidden
+    )
+
+  // Monitor bottom sheet state and emit event whenever the user dismisses the modal
+  LaunchedEffect(modalState) {
+    snapshotFlow { modalState.isVisible }
+      .collect { isVisible ->
+        // Toggle if state says the modal should be visible but the snapshot says it isn't.
+        if (state.petListFilterState.showBottomSheet && !isVisible)
+        eventSink( HomeScreen.Event.PetListFilterEvent(PetListFilterScreen.Event.ToggleAnimalFilter))
+      }
+  }
+
+  ModalBottomSheetLayout(
+    modifier = modifier,
+    sheetState = modalState,
+    sheetContent = {
+      Column {
+        GenderFilterOption(state.petListFilterState, eventSink)
+        SizeFilterOption(state.petListFilterState, eventSink)
       }
     }
   ) {
-    if (state.homeNavState.index == DOGS_SCREEN_INDEX) {
-      PetList(modifier = Modifier.padding(it), state = state.petListState) { event ->
-        eventSink(HomeScreen.Event.PetListEvent(event))
+    Scaffold(
+      modifier = modifier,
+      topBar = {
+        CenterAlignedTopAppBar(
+          title = {
+            Text("Adoptables", fontSize = 22.sp, color = MaterialTheme.colorScheme.onPrimaryContainer)
+          },
+          actions = {
+            IconButton(onClick = { eventSink( HomeScreen.Event.PetListFilterEvent(PetListFilterScreen.Event.ToggleAnimalFilter)) } ) {
+              Icon(imageVector = Icons.Default.FilterList, contentDescription = "filter pet list", tint = Color.White)
+            }
+          },
+          colors =
+          TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+          )
+        )
+      },
+      bottomBar = {
+        BottomNavigationBar(selectedIndex = state.homeNavState.index) { index ->
+          eventSink(HomeScreen.Event.HomeEvent(HomeNavScreen.Event.HomeNavEvent(index)))
+        }
       }
-    } else if (state.homeNavState.index == ABOUT_SCREEN_INDEX) {
-      About()
+    ) {
+      if (state.homeNavState.index == DOGS_SCREEN_INDEX) {
+        // I think CircuitContent might need to expose modifier.
+        CircuitContent(PetListScreen(gender = state.petListFilterState.gender, size = state.petListFilterState.size))
+
+//      PetListFilter(modifier = Modifier.padding(it), /*modalState = modalState,*/ state = state.petListFilterState) { event ->
+//        eventSink(HomeScreen.Event.PetListFilterEvent(event))
+//      }
+//
+//
+//      PetList(modifier = Modifier.padding(it), state = state.petListState) { event ->
+//        eventSink(HomeScreen.Event.PetListEvent(event))
+//      }
+
+      } else if (state.homeNavState.index == ABOUT_SCREEN_INDEX) {
+        About()
+      }
     }
   }
 }
@@ -171,3 +242,73 @@ private fun BottomNavigationBar(selectedIndex: Int, onSelectedIndex: (Int) -> Un
     }
   }
 }
+
+@Composable
+private fun GenderFilterOption(
+  state: PetListFilterScreen.State,
+  eventSink: (HomeScreen.Event) -> Unit
+) {
+  Box { Text(text = "Gender") }
+  Row(modifier = Modifier.selectableGroup()) {
+    Column {
+      Text(text = "All")
+      RadioButton(
+        selected = state.gender == Gender.ALL,
+        onClick = { eventSink(HomeScreen.Event.PetListFilterEvent(PetListFilterScreen.Event.FilterByGender(Gender.ALL)) ) }
+      )
+    }
+    Column {
+      Text(text = "Male")
+      RadioButton(
+        selected = state.gender == Gender.MALE,
+        onClick = { eventSink(HomeScreen.Event.PetListFilterEvent(PetListFilterScreen.Event.FilterByGender(Gender.MALE)) ) }
+      )
+    }
+    Column {
+      Text(text = "Female")
+      RadioButton(
+        selected = state.gender == Gender.FEMALE,
+        onClick = { eventSink(HomeScreen.Event.PetListFilterEvent(PetListFilterScreen.Event.FilterByGender(Gender.FEMALE)) ) }
+      )
+    }
+  }
+}
+
+@Composable
+private fun SizeFilterOption(
+  state: PetListFilterScreen.State,
+  eventSink: (HomeScreen.Event) -> Unit
+) {
+  Box { Text(text = "Size") }
+  Row(modifier = Modifier.selectableGroup()) {
+    Column {
+      Text(text = "All")
+      RadioButton(
+        selected = state.size == Size.ALL,
+        onClick = { eventSink(HomeScreen.Event.PetListFilterEvent(PetListFilterScreen.Event.FilterBySize(Size.ALL)) ) }
+      )
+    }
+    Column {
+      Text(text = "Small")
+      RadioButton(
+        selected = state.size == Size.SMALL,
+        onClick = { eventSink(HomeScreen.Event.PetListFilterEvent(PetListFilterScreen.Event.FilterBySize(Size.SMALL)) ) }
+      )
+    }
+    Column {
+      Text(text = "Medium")
+      RadioButton(
+        selected = state.size == Size.MEDIUM,
+        onClick = { eventSink(HomeScreen.Event.PetListFilterEvent(PetListFilterScreen.Event.FilterBySize(Size.MEDIUM)) ) }
+      )
+    }
+    Column {
+      Text(text = "Large")
+      RadioButton(
+        selected = state.size == Size.LARGE,
+        onClick = { eventSink(HomeScreen.Event.PetListFilterEvent(PetListFilterScreen.Event.FilterBySize(Size.LARGE)) ) }
+      )
+    }
+  }
+}
+
--- sample/src/main/kotlin/com/slack/circuit/sample/petlist/PetListFilterBottomSheet.kt
+++ sample/src/main/kotlin/com/slack/circuit/sample/petlist/PetListFilterBottomSheet.kt
@@ -1,38 +1,11 @@
 package com.slack.circuit.sample.petlist
 
 import android.os.Parcelable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.selection.selectableGroup
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.ModalBottomSheetLayout
-import androidx.compose.material.ModalBottomSheetValue
-import androidx.compose.material.Scaffold
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.FilterList
-import androidx.compose.material.rememberModalBottomSheetState
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.RadioButton
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshotFlow
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.sp
-import com.slack.circuit.CircuitContent
 import com.slack.circuit.EventCollector
 import com.slack.circuit.Navigator
 import com.slack.circuit.Presenter
@@ -41,20 +14,13 @@ import com.slack.circuit.Screen
 import com.slack.circuit.ScreenUi
 import com.slack.circuit.UiFactory
 import com.slack.circuit.sample.di.AppScope
-import com.slack.circuit.sample.petlist.Gender.ALL as ALL_GENDERS
-import com.slack.circuit.sample.petlist.Gender.FEMALE
-import com.slack.circuit.sample.petlist.Gender.MALE
-import com.slack.circuit.sample.petlist.Size.ALL as ALL_SIZES
-import com.slack.circuit.sample.petlist.Size.LARGE
-import com.slack.circuit.sample.petlist.Size.MEDIUM
-import com.slack.circuit.sample.petlist.Size.SMALL
 import com.slack.circuit.ui
 import com.squareup.anvil.annotations.ContributesMultibinding
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
-import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.parcelize.Parcelize
+import javax.inject.Inject
 
 @Parcelize
 object PetListFilterScreen : Screen {
@@ -120,136 +86,10 @@ class PetListFilterPresenter @AssistedInject constructor() :
 }
 
 private fun petListFilterUi() =
-  ui<PetListFilterScreen.State, PetListFilterScreen.Event> { state, events ->
-    PetListFilter(state, events)
+  ui<PetListFilterScreen.State, PetListFilterScreen.Event> { _, _ ->
+    PetListFilter()
   }
 
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
 internal fun PetListFilter(
-  state: PetListFilterScreen.State,
-  events: (PetListFilterScreen.Event) -> Unit
-) {
-  val modalState =
-    rememberModalBottomSheetState(
-      initialValue =
-        if (state.showBottomSheet) ModalBottomSheetValue.Expanded else ModalBottomSheetValue.Hidden
-    )
-
-  // Monitor bottom sheet state and emit event whenever the user dismisses the modal
-  LaunchedEffect(modalState) {
-    snapshotFlow { modalState.isVisible }
-      .collect { isVisible ->
-        // Toggle if state says the modal should be visible but the snapshot says it isn't.
-        if (state.showBottomSheet && !isVisible)
-          events(PetListFilterScreen.Event.ToggleAnimalFilter)
-      }
-  }
-
-  ModalBottomSheetLayout(
-    sheetState = modalState,
-    sheetContent = {
-      Column {
-        GenderFilterOption(state, events)
-        SizeFilterOption(state, events)
-      }
-    }
-  ) {
-    Scaffold(
-      modifier = Modifier.systemBarsPadding().fillMaxWidth(),
-      topBar = {
-        CenterAlignedTopAppBar(
-          title = {
-            androidx.compose.material.Text(
-              "Adoptables",
-              fontSize = 22.sp,
-              color = MaterialTheme.colorScheme.onPrimaryContainer
-            )
-          },
-          actions = {
-            IconButton(onClick = { events(PetListFilterScreen.Event.ToggleAnimalFilter) }) {
-              Icon(imageVector = Icons.Default.FilterList, contentDescription = "filter pet list", tint = Color.White)
-            }
-          },
-          colors =
-            TopAppBarDefaults.centerAlignedTopAppBarColors(
-              containerColor = MaterialTheme.colorScheme.primaryContainer
-            )
-        )
-      },
-    ) { paddingValues ->
-      Box(modifier = Modifier.padding(paddingValues)) {
-        CircuitContent(PetListScreen(gender = state.gender, size = state.size))
-      }
-    }
-  }
-}
-
-@Composable
-private fun GenderFilterOption(
-  state: PetListFilterScreen.State,
-  events: (PetListFilterScreen.Event) -> Unit
-) {
-  Box { Text(text = "Gender") }
-  Row(modifier = Modifier.selectableGroup()) {
-    Column {
-      Text(text = "All")
-      RadioButton(
-        selected = state.gender == ALL_GENDERS,
-        onClick = { events(PetListFilterScreen.Event.FilterByGender(ALL_GENDERS)) }
-      )
-    }
-    Column {
-      Text(text = "Male")
-      RadioButton(
-        selected = state.gender == MALE,
-        onClick = { events(PetListFilterScreen.Event.FilterByGender(MALE)) }
-      )
-    }
-    Column {
-      Text(text = "Female")
-      RadioButton(
-        selected = state.gender == FEMALE,
-        onClick = { events(PetListFilterScreen.Event.FilterByGender(FEMALE)) }
-      )
-    }
-  }
-}
-
-@Composable
-private fun SizeFilterOption(
-  state: PetListFilterScreen.State,
-  events: (PetListFilterScreen.Event) -> Unit
-) {
-  Box { Text(text = "Size") }
-  Row(modifier = Modifier.selectableGroup()) {
-    Column {
-      Text(text = "All")
-      RadioButton(
-        selected = state.size == ALL_SIZES,
-        onClick = { events(PetListFilterScreen.Event.FilterBySize(ALL_SIZES)) }
-      )
-    }
-    Column {
-      Text(text = "Small")
-      RadioButton(
-        selected = state.size == SMALL,
-        onClick = { events(PetListFilterScreen.Event.FilterBySize(SMALL)) }
-      )
-    }
-    Column {
-      Text(text = "Medium")
-      RadioButton(
-        selected = state.size == MEDIUM,
-        onClick = { events(PetListFilterScreen.Event.FilterBySize(MEDIUM)) }
-      )
-    }
-    Column {
-      Text(text = "Large")
-      RadioButton(
-        selected = state.size == LARGE,
-        onClick = { events(PetListFilterScreen.Event.FilterBySize(LARGE)) }
-      )
-    }
-  }
-}
+) {}

cdf7bef1318b21f0d43850d6aa1048f9eb916511
Zac Sweers <pandanomic@gmail.com>
2022-10-10 13:12:52 -0400

    Refactor petlist and home screen a bit (#188)

--- samples/star/src/test/kotlin/com/slack/circuit/star/petlist/PetListPresenterTest.kt
+++ samples/star/src/test/kotlin/com/slack/circuit/star/petlist/PetListPresenterTest.kt
@@ -38,7 +38,7 @@ class PetListPresenterTest {
   @Test
   fun `present - emit loading state then no animals state`() = runTest {
     val repository = TestRepository(emptyList())
-    val presenter = PetListPresenter(navigator, PetListScreen(), repository)
+    val presenter = PetListPresenter(navigator, repository)
 
     presenter.test {
       assertThat(awaitItem()).isEqualTo(PetListScreen.State.Loading)
@@ -49,7 +49,7 @@ class PetListPresenterTest {
   @Test
   fun `present - emit loading state then list of animals`() = runTest {
     val repository = TestRepository(listOf(animal))
-    val presenter = PetListPresenter(navigator, PetListScreen(), repository)
+    val presenter = PetListPresenter(navigator, repository)
 
     presenter.test {
       assertThat(awaitItem()).isEqualTo(PetListScreen.State.Loading)
@@ -64,7 +64,7 @@ class PetListPresenterTest {
   @Test
   fun `present - navigate to pet details screen`() = runTest {
     val repository = TestRepository(listOf(animal))
-    val presenter = PetListPresenter(navigator, PetListScreen(), repository)
+    val presenter = PetListPresenter(navigator, repository)
 
     presenter.test {
       assertThat(PetListScreen.State.Loading).isEqualTo(awaitItem())

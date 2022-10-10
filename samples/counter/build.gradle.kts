654e03041d4d3aa36ead22e86bdfcae7c94506c3
Zac Sweers <pandanomic@gmail.com>
2022-10-10 13:59:29 -0400

    spotless

--- samples/counter/build.gradle.kts
+++ samples/counter/build.gradle.kts
@@ -1,13 +1,26 @@
+/*
+ * Copyright (C) 2022 Slack Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import org.jetbrains.kotlin.gradle.plugin.PLUGIN_CLASSPATH_CONFIGURATION_NAME
 
-plugins {
-  kotlin("multiplatform")
-}
+plugins { kotlin("multiplatform") }
 
 kotlin {
-  //region KMP Targets
+  // region KMP Targets
   jvm()
-  //endregion
+  // endregion
 
   sourceSets {
     commonMain {
@@ -16,14 +29,8 @@ kotlin {
         api(libs.coroutines)
       }
     }
-    maybeCreate("commonTest").apply {
-      dependencies {
-        implementation(libs.kotlin.test)
-      }
-    }
+    maybeCreate("commonTest").apply { dependencies { implementation(libs.kotlin.test) } }
   }
 }
 
-dependencies {
-  add(PLUGIN_CLASSPATH_CONFIGURATION_NAME, libs.androidx.compose.compiler)
-}
+dependencies { add(PLUGIN_CLASSPATH_CONFIGURATION_NAME, libs.androidx.compose.compiler) }
--- samples/counter/mosaic/build.gradle.kts
+++ samples/counter/mosaic/build.gradle.kts
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2022 Slack Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import org.jetbrains.kotlin.gradle.plugin.PLUGIN_CLASSPATH_CONFIGURATION_NAME
 
 plugins {
@@ -7,9 +22,7 @@ plugins {
   `java-library`
 }
 
-application {
-  mainClass.set("com.slack.circuit.sample.counter.mosaic.MosaicCounterCircuitKt")
-}
+application { mainClass.set("com.slack.circuit.sample.counter.mosaic.MosaicCounterCircuitKt") }
 
 dependencies {
   implementation(projects.samples.counter)

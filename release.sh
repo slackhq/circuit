a754ca06fe60676ea8f448d76bbf92ad07700827
Zac Sweers <pandanomic@gmail.com>
2022-10-10 13:18:19 -0400

    Prepare next development version.

--- release.sh
+++ release.sh
@@ -19,7 +19,7 @@ git commit -am "Prepare for release $NEW_VERSION."
 git tag -a "$NEW_VERSION" -m "Version $NEW_VERSION"
 
 # Publish
-./gradlew publish -x dokkaHtml
+./gradlew publish -x dokkaHtml --no-configuration-cache
 
 # Prepare next snapshot
 echo "Restoring snapshot version $SNAPSHOT_VERSION"

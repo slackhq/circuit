8e527c1910cca670736d8f5c7c5f1fa7fb8fdf0c
Zac Sweers <pandanomic@gmail.com>
2022-10-10 13:57:58 -0400

    Fixup spotless config + correctly format build files (#193)

--- circuit-test/build.gradle.kts
+++ circuit-test/build.gradle.kts
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2022 Slack Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import org.jetbrains.kotlin.gradle.plugin.PLUGIN_CLASSPATH_CONFIGURATION_NAME
 
 plugins {
@@ -7,12 +22,10 @@ plugins {
 }
 
 kotlin {
-  //region KMP Targets
-  android {
-    publishLibraryVariants("release")
-  }
+  // region KMP Targets
+  android { publishLibraryVariants("release") }
   jvm()
-  //endregion
+  // endregion
 
   sourceSets {
     commonMain {
@@ -24,18 +37,14 @@ kotlin {
         api(libs.molecule.runtime)
       }
     }
-//    maybeCreate("androidMain").apply {
-//      dependencies {
-//        api(libs.bundles.compose)
-//      }
-//    }
+    //    maybeCreate("androidMain").apply {
+    //      dependencies {
+    //        api(libs.bundles.compose)
+    //      }
+    //    }
   }
 }
 
-android {
-  namespace = "com.slack.circuit.test"
-}
+android { namespace = "com.slack.circuit.test" }
 
-dependencies {
-  add(PLUGIN_CLASSPATH_CONFIGURATION_NAME, libs.androidx.compose.compiler)
-}
+dependencies { add(PLUGIN_CLASSPATH_CONFIGURATION_NAME, libs.androidx.compose.compiler) }

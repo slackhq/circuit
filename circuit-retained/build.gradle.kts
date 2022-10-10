9fab4c2f99fd174682882b44a87b92db6b5351c8
Zac Sweers <pandanomic@gmail.com>
2022-10-10 00:32:55 -0400

    Fixup spotless config + correctly format build files

--- circuit-retained/build.gradle.kts
+++ circuit-retained/build.gradle.kts
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2022 Slack Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import org.jetbrains.kotlin.gradle.plugin.PLUGIN_CLASSPATH_CONFIGURATION_NAME
 
 plugins {
@@ -7,12 +22,10 @@ plugins {
 }
 
 kotlin {
-  //region KMP Targets
-  android {
-    publishLibraryVariants("release")
-  }
+  // region KMP Targets
+  android { publishLibraryVariants("release") }
   jvm()
-  //endregion
+  // endregion
 
   sourceSets {
     commonMain {
@@ -28,27 +41,18 @@ kotlin {
         api(libs.bundles.compose)
       }
     }
-    maybeCreate("commonTest").apply {
-      dependencies {
-        implementation(libs.kotlin.test)
+    maybeCreate("commonTest").apply { dependencies { implementation(libs.kotlin.test) } }
+    val commonJvmTest =
+      maybeCreate("commonJvmTest").apply {
+        dependencies {
+          implementation(libs.junit)
+          implementation(libs.truth)
+        }
       }
-    }
-    val commonJvmTest = maybeCreate("commonJvmTest").apply {
-      dependencies {
-        implementation(libs.junit)
-        implementation(libs.truth)
-      }
-    }
-    maybeCreate("jvmTest").apply {
-      dependsOn(commonJvmTest)
-    }
+    maybeCreate("jvmTest").apply { dependsOn(commonJvmTest) }
   }
 }
 
-android {
-  namespace = "com.slack.circuit.retained"
-}
+android { namespace = "com.slack.circuit.retained" }
 
-dependencies {
-  add(PLUGIN_CLASSPATH_CONFIGURATION_NAME, libs.androidx.compose.compiler)
-}
+dependencies { add(PLUGIN_CLASSPATH_CONFIGURATION_NAME, libs.androidx.compose.compiler) }

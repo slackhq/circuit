98f3798b2159a32960e4f4a51c5aa67d2a0329e0
Zac Sweers <pandanomic@gmail.com>
2022-10-12 00:33:24 -0400

    Cleanups + add preview

--- samples/star/src/main/kotlin/com/slack/circuit/star/petlist/PetList.kt
+++ samples/star/src/main/kotlin/com/slack/circuit/star/petlist/PetList.kt
@@ -46,6 +46,7 @@ import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -65,6 +66,7 @@ import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.capitalize
 import androidx.compose.ui.text.intl.Locale
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
@@ -271,8 +273,8 @@ internal object PetListTestConstants {
 
 @Composable
 internal fun PetList(
-  modifier: Modifier = Modifier,
   state: PetListScreen.State,
+  modifier: Modifier = Modifier,
 ) {
   if (state is PetListScreen.State.Success && state.isUpdateFiltersModalShowing) {
     val eventSink = state.eventSink
@@ -333,11 +335,11 @@ internal fun PetList(
 }
 
 @Composable
-internal fun PetListGrid(
-  modifier: Modifier = Modifier,
+private fun PetListGrid(
   animals: List<PetListAnimal>,
   isRefreshing: Boolean,
-  eventSink: (PetListScreen.Event) -> Unit
+  modifier: Modifier = Modifier,
+  eventSink: (PetListScreen.Event) -> Unit,
 ) {
   SwipeRefresh(
     state = rememberSwipeRefreshState(isRefreshing = isRefreshing),
@@ -365,7 +367,7 @@ internal fun PetListGrid(
 }
 
 @Composable
-internal fun PetListGridItem(animal: PetListAnimal, onClick: () -> Unit = {}) {
+private fun PetListGridItem(animal: PetListAnimal, onClick: () -> Unit = {}) {
   ElevatedCard(
     modifier = Modifier.fillMaxWidth().testTag(CARD_TAG),
     shape = RoundedCornerShape(16.dp),
@@ -418,8 +420,14 @@ private suspend fun OverlayHost.updateFilters(currentFilters: Filters): Filters 
   )
 }
 
+@Preview
 @Composable
-internal fun UpdateFiltersSheet(initialFilters: Filters, onDismiss: (Filters) -> Unit = {}) {
+internal fun PreviewUpdateFiltersSheet() {
+  Surface { UpdateFiltersSheet(initialFilters = Filters()) }
+}
+
+@Composable
+private fun UpdateFiltersSheet(initialFilters: Filters, onDismiss: (Filters) -> Unit = {}) {
   var filters by remember { mutableStateOf(initialFilters) }
   Column(Modifier.fillMaxWidth()) {
     GenderFilterOption(filters.gender) { filters = filters.copy(gender = it) }

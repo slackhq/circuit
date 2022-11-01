d974fb38d0e9adf475b845b875312edbe500c602
Zac Sweers <pandanomic@gmail.com>
2022-11-01 09:10:20 -0700

    Implement basic interop docs + sample (#251)

---
+++ samples/interop/src/main/kotlin/com/slack/circuit/sample/interop/ComposeCounterUi.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2022 Slack Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.slack.circuit.sample.interop
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.unit.dp
+import com.slack.circuit.sample.counter.CounterEvent
+import com.slack.circuit.sample.counter.CounterState
+
+/** A standard Circuit Counter UI in Compose. */
+@Composable
+fun Counter(state: CounterState, modifier: Modifier = Modifier) {
+  val color = if (state.count >= 0) Color.Unspecified else MaterialTheme.colorScheme.error
+  Box(modifier.fillMaxSize()) {
+    Column(Modifier.align(Alignment.Center)) {
+      Text(
+        modifier = Modifier.align(Alignment.CenterHorizontally),
+        text = "Count: ${state.count}",
+        style = MaterialTheme.typography.displayLarge,
+        color = color
+      )
+      Spacer(modifier = Modifier.height(16.dp))
+      val sink = state.eventSink
+      Button(
+        modifier = Modifier.align(Alignment.CenterHorizontally),
+        onClick = { sink(CounterEvent.Increment) }
+      ) {
+        Icon(rememberVectorPainter(Icons.Filled.Add), "Increment")
+      }
+      Button(
+        modifier = Modifier.align(Alignment.CenterHorizontally),
+        onClick = { sink(CounterEvent.Decrement) }
+      ) {
+        Icon(rememberVectorPainter(Icons.Filled.Remove), "Decrement")
+      }
+    }
+  }
+}

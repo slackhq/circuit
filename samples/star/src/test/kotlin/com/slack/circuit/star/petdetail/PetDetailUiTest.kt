69b487d61cb8bd1b517f982f6da81175a94c54f0
Zac Sweers <pandanomic@gmail.com>
2022-11-01 15:03:11 -0700

    Move PetDetail and PetList UI tests to Robolectric (#248)
    This runs _way_ faster and gives us quick headless UI testing.
    
    Open question – do we drop the UI tests entirely? Would be nice to run
    both but alas there's no test fixtures support still for kotlin android
    projects

---
+++ samples/star/src/test/kotlin/com/slack/circuit/star/petdetail/PetDetailUiTest.kt
@@ -0,0 +1,128 @@
+/*
+ * Copyright (C) 2022 Slack Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.slack.circuit.star.petdetail
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTouchInput
+import androidx.compose.ui.test.swipeUp
+import androidx.test.platform.app.InstrumentationRegistry
+import coil.Coil
+import com.google.common.truth.Truth.assertThat
+import com.slack.circuit.CircuitCompositionLocals
+import com.slack.circuit.CircuitConfig
+import com.slack.circuit.star.R
+import com.slack.circuit.star.petdetail.PetDetailTestConstants.ANIMAL_CONTAINER_TAG
+import com.slack.circuit.star.petdetail.PetDetailTestConstants.PROGRESS_TAG
+import com.slack.circuit.star.petdetail.PetDetailTestConstants.UNKNOWN_ANIMAL_TAG
+import com.slack.circuit.star.petdetail.PetPhotoCarouselTestConstants.CAROUSEL_TAG
+import com.slack.circuit.star.ui.FakeImageLoader
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class PetDetailUiTest {
+  @get:Rule val composeTestRule = createComposeRule()
+
+  @Before
+  fun setup() {
+    val fakeImageLoader =
+      FakeImageLoader(
+        InstrumentationRegistry.getInstrumentation().targetContext.getDrawable(R.drawable.dog2)!!
+      )
+    Coil.setImageLoader(fakeImageLoader)
+  }
+
+  @Test
+  fun petDetail_show_progress_indicator_for_loading_state() {
+    composeTestRule.run {
+      setContent { PetDetail(PetDetailScreen.State.Loading) }
+
+      onNodeWithTag(PROGRESS_TAG).assertIsDisplayed()
+      onNodeWithTag(UNKNOWN_ANIMAL_TAG).assertDoesNotExist()
+      onNodeWithTag(ANIMAL_CONTAINER_TAG).assertDoesNotExist()
+    }
+  }
+
+  @Test
+  fun petDetail_show_message_for_unknown_animal_state() {
+    composeTestRule.run {
+      setContent { PetDetail(PetDetailScreen.State.UnknownAnimal) }
+
+      onNodeWithTag(PROGRESS_TAG).assertDoesNotExist()
+      onNodeWithTag(ANIMAL_CONTAINER_TAG).assertDoesNotExist()
+
+      onNodeWithTag(UNKNOWN_ANIMAL_TAG)
+        .assertIsDisplayed()
+        .assertTextEquals(
+          InstrumentationRegistry.getInstrumentation()
+            .targetContext
+            .getString(R.string.unknown_animals)
+        )
+    }
+  }
+
+  @Test
+  fun petDetail_show_animal_for_success_state() {
+    val success =
+      PetDetailScreen.State.Success(
+        url = "url",
+        photoUrls = listOf("http://some.url"),
+        photoUrlMemoryCacheKey = null,
+        name = "Baxter",
+        description = "Grumpy looking Australian Terrier",
+        tags = listOf("dog", "terrier", "male"),
+      )
+
+    var carouselScreen: PetPhotoCarouselScreen? = null
+    val circuitConfig =
+      CircuitConfig.Builder()
+        .setOnUnavailableContent { screen ->
+          carouselScreen = screen as PetPhotoCarouselScreen
+          PetPhotoCarousel(PetPhotoCarouselScreen.State(screen))
+        }
+        .build()
+
+    val expectedScreen =
+      PetPhotoCarouselScreen(
+        name = success.name,
+        photoUrls = success.photoUrls,
+        photoUrlMemoryCacheKey = null
+      )
+
+    composeTestRule.run {
+      setContent { CircuitCompositionLocals(circuitConfig) { PetDetail(success) } }
+
+      onNodeWithTag(PROGRESS_TAG).assertDoesNotExist()
+      onNodeWithTag(UNKNOWN_ANIMAL_TAG).assertDoesNotExist()
+
+      onNodeWithTag(CAROUSEL_TAG).assertIsDisplayed().performTouchInput { swipeUp() }
+      onNodeWithText(success.name).assertIsDisplayed()
+      onNodeWithText(success.description).assertIsDisplayed()
+
+      assertThat(carouselScreen).run {
+        isNotNull()
+        isEqualTo(expectedScreen)
+      }
+    }
+  }
+}
---
+++ samples/star/src/test/kotlin/com/slack/circuit/star/petlist/PetListUiTest.kt
@@ -0,0 +1,134 @@
+/*
+ * Copyright (C) 2022 Slack Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.slack.circuit.star.petlist
+
+import androidx.compose.ui.test.assertCountEquals
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithTag
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.test.platform.app.InstrumentationRegistry
+import coil.Coil
+import com.google.common.truth.Truth.assertThat
+import com.slack.circuit.star.R
+import com.slack.circuit.star.petlist.PetListTestConstants.CARD_TAG
+import com.slack.circuit.star.petlist.PetListTestConstants.GRID_TAG
+import com.slack.circuit.star.petlist.PetListTestConstants.IMAGE_TAG
+import com.slack.circuit.star.petlist.PetListTestConstants.NO_ANIMALS_TAG
+import com.slack.circuit.star.petlist.PetListTestConstants.PROGRESS_TAG
+import com.slack.circuit.star.ui.FakeImageLoader
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class PetListUiTest {
+  @get:Rule val composeTestRule = createComposeRule()
+
+  @Before
+  fun setup() {
+    val fakeImageLoader =
+      FakeImageLoader(
+        InstrumentationRegistry.getInstrumentation().targetContext.getDrawable(R.drawable.dog)!!
+      )
+    Coil.setImageLoader(fakeImageLoader)
+  }
+
+  @Test
+  fun petList_show_progress_indicator_for_loading_state() {
+    composeTestRule.run {
+      setContent { PetList(PetListScreen.State.Loading) }
+
+      onNodeWithTag(PROGRESS_TAG).assertIsDisplayed()
+      onNodeWithTag(NO_ANIMALS_TAG).assertDoesNotExist()
+      onNodeWithTag(GRID_TAG).assertDoesNotExist()
+    }
+  }
+
+  @Test
+  fun petList_show_message_for_no_animals_state() {
+    composeTestRule.run {
+      setContent { PetList(PetListScreen.State.NoAnimals(isRefreshing = false)) }
+
+      onNodeWithTag(PROGRESS_TAG).assertDoesNotExist()
+      onNodeWithTag(GRID_TAG).assertDoesNotExist()
+
+      onNodeWithTag(NO_ANIMALS_TAG)
+        .assertIsDisplayed()
+        .assertTextEquals(
+          InstrumentationRegistry.getInstrumentation().targetContext.getString(R.string.no_animals)
+        )
+    }
+  }
+
+  @Test
+  fun petList_show_list_for_success_state() {
+    val animals = persistentListOf(ANIMAL)
+
+    composeTestRule.run {
+      setContent { PetList(PetListScreen.State.Success(animals, isRefreshing = false) {}) }
+
+      onNodeWithTag(PROGRESS_TAG).assertDoesNotExist()
+      onNodeWithTag(NO_ANIMALS_TAG).assertDoesNotExist()
+
+      onAllNodesWithTag(CARD_TAG).assertCountEquals(1)
+      onNodeWithTag(IMAGE_TAG, true).assertIsDisplayed()
+      onNodeWithText(ANIMAL.name).assertIsDisplayed()
+      onNodeWithText(ANIMAL.breed.orEmpty()).assertIsDisplayed()
+      onNodeWithText("${ANIMAL.gender} – ${ANIMAL.age}").assertIsDisplayed()
+    }
+  }
+
+  @Test
+  fun petList_emits_event_when_tapping_on_animal() = runTest {
+    val channel = Channel<Any>(1)
+    val animals = persistentListOf(ANIMAL)
+
+    composeTestRule.run {
+      setContent {
+        PetList(
+          PetListScreen.State.Success(animals, isRefreshing = false, eventSink = channel::trySend)
+        )
+      }
+
+      onAllNodesWithTag(CARD_TAG).assertCountEquals(1)[0].performClick()
+
+      val event = channel.receive()
+      assertThat(event).isEqualTo(PetListScreen.Event.ClickAnimal(ANIMAL.id, ANIMAL.imageUrl))
+    }
+  }
+
+  private companion object {
+    val ANIMAL =
+      PetListAnimal(
+        id = 1L,
+        name = "Baxter",
+        imageUrl = "http://some.url",
+        breed = "Australian Terrier",
+        gender = "male",
+        size = "small",
+        age = "12"
+      )
+  }
+}

50ae3e00a6a7ec0ab5ac3d29dae241f1ea8808bf
Amrita Venkatraman <avenkatraman@slack-corp.com>
2022-11-01 12:39:00 -0700

    spotless apply for benchmark

--- samples/star/benchmark/build.gradle.kts
+++ samples/star/benchmark/build.gradle.kts
@@ -13,42 +13,40 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import com.android.build.api.dsl.ManagedVirtualDevice
-
 plugins {
-    id("com.android.test")
-    kotlin("android")
+  id("com.android.test")
+  kotlin("android")
 }
 
 android {
-    namespace = "com.circuit.samples.star.benchmark"
-    defaultConfig {
-        targetSdk = 33
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
+  namespace = "com.circuit.samples.star.benchmark"
+  defaultConfig {
+    targetSdk = 33
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
 
-    buildTypes {
-        // This benchmark buildType is used for benchmarking, and should function like your
-        // release build (for example, with minification on). It"s signed with a debug key
-        // for easy local/CI testing.
-        create("benchmark") {
-            isDebuggable = true
-            signingConfig = getByName("debug").signingConfig
-            matchingFallbacks += listOf("release")
-        }
+  buildTypes {
+    // This benchmark buildType is used for benchmarking, and should function like your
+    // release build (for example, with minification on). It"s signed with a debug key
+    // for easy local/CI testing.
+    create("benchmark") {
+      isDebuggable = true
+      signingConfig = getByName("debug").signingConfig
+      matchingFallbacks += listOf("release")
     }
+  }
 
-    targetProjectPath = ":samples:star:apk"
-    experimentalProperties["android.experimental.self-instrumenting"] = true
+  targetProjectPath = ":samples:star:apk"
+  experimentalProperties["android.experimental.self-instrumenting"] = true
 }
 
 dependencies {
-    implementation(libs.androidx.compose.runtime)
-    implementation(libs.androidx.test.ext.junit)
-    implementation(libs.androidx.test.espresso.core)
-    implementation(libs.androidx.test.uiautomator)
-    implementation(libs.androidx.benchmark.macro.junit)
-    implementation(libs.androidx.profileinstaller)
+  implementation(libs.androidx.compose.runtime)
+  implementation(libs.androidx.test.ext.junit)
+  implementation(libs.androidx.test.espresso.core)
+  implementation(libs.androidx.test.uiautomator)
+  implementation(libs.androidx.benchmark.macro.junit)
+  implementation(libs.androidx.profileinstaller)
 }
 
 androidComponents { beforeVariants { it.enable = it.buildType == "benchmark" } }
--- samples/star/benchmark/src/main/java/com/example/benchmark/StartupBenchmark.kt
+++ samples/star/benchmark/src/main/java/com/example/benchmark/StartupBenchmark.kt
@@ -23,7 +23,6 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-
 @RunWith(AndroidJUnit4::class)
 class StartupBenchmark {
   @get:Rule val benchmarkRule = MacrobenchmarkRule()

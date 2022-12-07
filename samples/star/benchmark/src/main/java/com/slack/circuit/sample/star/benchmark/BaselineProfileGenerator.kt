0b492f0a973fc02076c4cad92a69e73f13d89065
Zac Sweers <pandanomic@gmail.com>
2022-12-07 16:29:24 -0500

    Update benchmark and regenerate baseline profiles (#326)

--- samples/star/benchmark/src/main/java/com/slack/circuit/sample/star/benchmark/BaselineProfileGenerator.kt
+++ samples/star/benchmark/src/main/java/com/slack/circuit/sample/star/benchmark/BaselineProfileGenerator.kt
@@ -1,6 +1,6 @@
 // Copyright (C) 2022 Slack Technologies, LLC
 // SPDX-License-Identifier: Apache-2.0
-package com.example.benchmark
+package com.slack.circuit.sample.star.benchmark
 
 import androidx.benchmark.macro.junit4.BaselineProfileRule
 import org.junit.Rule
@@ -10,10 +10,14 @@ class BaselineProfileGenerator {
   @get:Rule val baselineProfileRule = BaselineProfileRule()
 
   @Test
-  fun startup() =
+  fun startupBaselineProfile() =
     baselineProfileRule.collectBaselineProfile(
       packageName = "com.slack.circuit.sample.star.apk",
-      packageFilters = listOf("com.slack.circuit"),
+      filterPredicate = { rule ->
+        rule.contains("com/slack/circuit") &&
+          !rule.contains("com/slack/circuit/sample") &&
+          !rule.contains("com/slack/circuit/star")
+      },
       profileBlock = { startActivityAndWait() }
     )
 }

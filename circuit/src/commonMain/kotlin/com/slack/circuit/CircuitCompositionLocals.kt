137a0e3aed03e58128b601587d34a7c6b4e77cb4
Zac Sweers <pandanomic@gmail.com>
2022-12-22 15:58:29 -0500

    Make LocalCircuitConfig more idiomatic (#355)
    This isn't an "owner" type and should just be a standard
    `LocalCircuitConfig` convention

--- circuit/src/commonMain/kotlin/com/slack/circuit/CircuitCompositionLocals.kt
+++ circuit/src/commonMain/kotlin/com/slack/circuit/CircuitCompositionLocals.kt
@@ -5,7 +5,7 @@ package com.slack.circuit
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocal
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.ProvidedValue
+import androidx.compose.runtime.ProvidableCompositionLocal
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.staticCompositionLocalOf
 
@@ -16,7 +16,7 @@ import androidx.compose.runtime.staticCompositionLocalOf
 @Composable
 public fun CircuitCompositionLocals(circuitConfig: CircuitConfig, content: @Composable () -> Unit) {
   CompositionLocalProvider(
-    LocalCircuitOwner provides circuitConfig,
+    LocalCircuitConfig provides circuitConfig,
   ) {
     content()
   }
@@ -24,17 +24,8 @@ public fun CircuitCompositionLocals(circuitConfig: CircuitConfig, content: @Comp
 
 internal val LocalCircuitContext = compositionLocalOf<CircuitContext?> { null }
 
-public object LocalCircuitOwner {
-  private val LocalCircuitConfig = staticCompositionLocalOf<CircuitConfig?> { null }
-
-  /**
-   * Returns current composition local value for the owner or errors if one has not been provided.
-   */
-  public val current: CircuitConfig
-    @Composable get() = LocalCircuitConfig.current ?: error("No circuit available")
-
-  /** Associates a [LocalCircuitConfig] key to a value in a call to [CompositionLocalProvider]. */
-  public infix fun provides(circuitConfig: CircuitConfig): ProvidedValue<CircuitConfig?> {
-    return LocalCircuitConfig.provides(circuitConfig)
+/** CompositionLocal with a current [CircuitConfig] instance. */
+public val LocalCircuitConfig: ProvidableCompositionLocal<CircuitConfig?> =
+  staticCompositionLocalOf {
+    null
   }
-}

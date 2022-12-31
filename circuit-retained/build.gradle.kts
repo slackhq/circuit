bcecc40781542bdd3c0b203923cabcdc24ed059a
Zac Sweers <pandanomic@gmail.com>
2022-12-31 13:28:32 -0400

    Merge retained android tests into core retained artifact again

--- circuit-retained/build.gradle.kts
+++ circuit-retained/build.gradle.kts
@@ -20,13 +20,14 @@ kotlin {
         api(libs.coroutines)
       }
     }
-    maybeCreate("androidMain").apply {
-      dependencies {
-        api(libs.androidx.lifecycle.viewModel.compose)
-        api(libs.androidx.lifecycle.viewModel)
-        api(libs.androidx.compose.runtime)
+    val androidMain =
+      maybeCreate("androidMain").apply {
+        dependencies {
+          api(libs.androidx.lifecycle.viewModel.compose)
+          api(libs.androidx.lifecycle.viewModel)
+          api(libs.androidx.compose.runtime)
+        }
       }
-    }
     maybeCreate("commonTest").apply { dependencies { implementation(libs.kotlin.test) } }
     val commonJvmTest =
       maybeCreate("commonJvmTest").apply {
@@ -38,7 +39,34 @@ kotlin {
     maybeCreate("jvmTest").apply { dependsOn(commonJvmTest) }
     // TODO export this in Android too when it's supported in kotlin projects
     maybeCreate("jvmMain").apply { dependencies.add("testFixturesApi", projects.circuitTest) }
+    maybeCreate("androidInstrumentedTest").apply {
+      dependsOn(androidMain)
+      dependsOn(commonJvmTest)
+      dependencies {
+        implementation(libs.coroutines)
+        implementation(libs.coroutines.android)
+        implementation(projects.circuitRetained)
+        implementation(libs.androidx.compose.integration.activity)
+        implementation(libs.androidx.compose.ui.testing.junit)
+        implementation(libs.androidx.compose.foundation)
+        implementation(libs.androidx.compose.ui.ui)
+        implementation(libs.androidx.compose.material.material)
+        implementation(libs.leakcanary.android.instrumentation)
+        implementation(libs.junit)
+        implementation(libs.truth)
+      }
+    }
   }
 }
 
-android { namespace = "com.slack.circuit.retained" }
+android {
+  namespace = "com.slack.circuit.retained"
+
+  defaultConfig {
+    minSdk = 28
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+
+  testOptions { unitTests.isIncludeAndroidResources = true }
+  testBuildType = "release"
+}

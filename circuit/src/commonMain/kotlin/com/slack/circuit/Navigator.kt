b3026af993f98cd322ebbde81d3f7c4de8615cdc
Ash Davies <1892070+ashdavies@users.noreply.github.com>
2023-02-02 22:32:07 +0100

    Navigation Common (#425)
    This change goes ahead with moving what can be moved to the common
    source set, in some instances where Android platform specific things are
    required, the original method signature has been preserved and delegates
    to the common function.
    
    - Compose UI dependency introduces to *backstack* module
    - `fun BasicNavigableCircuitContent` moved to `commonMain`
    - `object NavigatorDefaults` moved to `commonMain`
    - `fun rememberCircuitNavigator` moved to `commonMain`
    - `NavigatorImpl` moved to `commonMain`
    
    With the latter `rememberCircuitNavigator()` the `onRootPop` parameter
    has been removed for the android source set where it calls the method
    with the original signature in the common source set with the provided
    Android back stack handler.
    
    ---------
    
    Co-authored-by: Davies Ashley <ashdavies@users.noreply.github.com>

--- circuit/src/commonMain/kotlin/com/slack/circuit/Navigator.kt
+++ circuit/src/commonMain/kotlin/com/slack/circuit/Navigator.kt
@@ -2,7 +2,13 @@
 // SPDX-License-Identifier: Apache-2.0
 package com.slack.circuit
 
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.remember
+import com.slack.circuit.backstack.SaveableBackStack
+import com.slack.circuit.backstack.isAtRoot
+import com.slack.circuit.backstack.isEmpty
+import com.slack.circuit.backstack.popUntil
 
 /** A basic navigation interface for navigating between [screens][Screen]. */
 @Stable
@@ -65,5 +71,74 @@ public fun Navigator.popUntil(predicate: (Screen) -> Boolean) {
     if (predicate(screen)) {
       break
     }
+  }
+}
+
+/**
+ * Returns a new [Navigator] for navigating within [CircuitContents][CircuitContent].
+ *
+ * @param backstack The backing [SaveableBackStack] to navigate.
+ * @param onRootPop Invoked when the backstack is at root (size 1) and the user presses the back
+ *   button.
+ * @see BasicNavigableCircuitContent
+ */
+@Composable
+public fun rememberCircuitNavigator(
+  backstack: SaveableBackStack,
+  onRootPop: () -> Unit,
+): Navigator {
+  return remember { NavigatorImpl(backstack, onRootPop) }
+}
+
+internal class NavigatorImpl(
+  private val backstack: SaveableBackStack,
+  private val onRootPop: () -> Unit
+) : Navigator {
+
+  init {
+    check(!backstack.isEmpty) { "Backstack size must not be empty." }
+  }
+
+  override fun goTo(screen: Screen) = backstack.push(screen)
+
+  override fun pop(): Screen? {
+    if (backstack.isAtRoot) {
+      onRootPop()
+      return null
+    }
+
+    return backstack.pop()?.screen
+  }
+
+  override fun resetRoot(newRoot: Screen): List<Screen> {
+    return buildList(backstack.size) {
+      backstack.popUntil { record ->
+        add(record.screen)
+        false
+      }
+      backstack.push(newRoot)
+    }
+  }
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (javaClass != other?.javaClass) return false
+
+    other as NavigatorImpl
+
+    if (backstack != other.backstack) return false
+    if (onRootPop != other.onRootPop) return false
+
+    return true
+  }
+
+  override fun hashCode(): Int {
+    var result = backstack.hashCode()
+    result = 31 * result + onRootPop.hashCode()
+    return result
+  }
+
+  override fun toString(): String {
+    return "NavigatorImpl(backstack=$backstack, onRootPop=$onRootPop)"
   }
 }

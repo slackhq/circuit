63abce70ea4d82750d53610de3851d588a4db08b
Kieran Elliott <kierse@pissiphany.com>
2023-02-27 11:09:29 -0500

    Manifest updates

--- samples/wizard/build.gradle.kts
+++ samples/wizard/build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
 
   implementation(libs.androidx.activity.ktx)
   implementation(libs.androidx.compose.integration.activity)
+  implementation(libs.androidx.compose.integration.materialThemeAdapter)
   implementation(libs.androidx.compose.material.material3)
   implementation(libs.bundles.compose.ui)
   implementation(libs.dagger)

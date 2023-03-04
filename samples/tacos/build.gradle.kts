3f240bf835768d6b995e38085830c2dd21fc7172
Kieran Elliott <kierse@pissiphany.com>
2023-03-04 16:55:03 -0500

    Adding empty tacos module

---
+++ samples/tacos/build.gradle.kts
@@ -0,0 +1,68 @@
+import org.jetbrains.kotlin.gradle.internal.KaptGenerateStubsTask
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+  id("com.android.application")
+  kotlin("android")
+  kotlin("kapt")
+  kotlin("plugin.parcelize")
+  alias(libs.plugins.ksp)
+}
+
+android {
+  namespace = "com.slack.circuit.tacos"
+
+  defaultConfig {
+    minSdk = 33
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+}
+
+tasks
+  .withType<KotlinCompile>()
+  .matching { it !is KaptGenerateStubsTask }
+  .configureEach {
+    compilerOptions {
+      freeCompilerArgs.addAll(
+        "-opt-in=androidx.compose.material.ExperimentalMaterialApi",
+        "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api",
+        "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi"
+      )
+
+      if (project.hasProperty("circuit.enableComposeCompilerReports")) {
+        freeCompilerArgs.addAll(
+          "-P",
+          "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=" +
+            project.buildDir.absolutePath +
+            "/compose_metrics"
+        )
+        freeCompilerArgs.addAll(
+          "-P",
+          "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=" +
+            project.buildDir.absolutePath +
+            "/compose_metrics"
+        )
+      }
+    }
+  }
+
+dependencies {
+  kapt(libs.dagger.compiler)
+  ksp(projects.circuitCodegen)
+
+  implementation(libs.androidx.activity.ktx)
+  implementation(libs.androidx.appCompat)
+  implementation(libs.androidx.compose.integration.activity)
+  implementation(libs.androidx.compose.integration.materialThemeAdapter)
+  implementation(libs.androidx.compose.material.material3)
+  implementation(libs.bundles.compose.ui)
+  implementation(libs.dagger)
+  implementation(libs.kotlinx.immutable)
+  implementation(projects.circuit)
+  implementation(projects.circuitCodegenAnnotations)
+
+  testImplementation(libs.junit)
+
+  androidTestImplementation(libs.androidx.test.ext.junit)
+  androidTestImplementation(libs.testing.espresso.core)
+}
---
+++ samples/tacos/src/main/kotlin/com/slack/circuit/tacos/MainActivity.kt
@@ -0,0 +1,7 @@
+// Copyright (C) 2022 Slack Technologies, LLC
+// SPDX-License-Identifier: Apache-2.0
+package com.slack.circuit.tacos
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity()

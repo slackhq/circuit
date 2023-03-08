ce921be240821d956d84a7ca25e4e80ef369b559
Kieran Elliott <kierse@pissiphany.com>
2023-03-08 15:49:11 -0500

    Adding ToppingsOrderStep

---
+++ samples/tacos/src/main/kotlin/com/slack/circuit/tacos/step/ToppingsOrderStep.kt
@@ -0,0 +1,13 @@
+package com.slack.circuit.tacos.step
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+object ToppingsOrderStep : OrderStep {
+  override val number = 1
+}
+
+@Composable
+fun ToppingsUi(state: ToppingsProducer.State, modifier: Modifier = Modifier) {
+
+}

a2e7593f96d29b2c45df6140cc3b7e87931b13eb
Kieran Elliott <kierse@pissiphany.com>
2023-03-07 19:30:18 -0500

    Added Fillings UI

--- samples/tacos/src/main/kotlin/com/slack/circuit/tacos/step/FillingsOrderStep.kt
+++ samples/tacos/src/main/kotlin/com/slack/circuit/tacos/step/FillingsOrderStep.kt
@@ -1,16 +1,23 @@
 package com.slack.circuit.tacos.step
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.produceState
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.slack.circuit.tacos.OrderDetails
+import com.slack.circuit.tacos.model.Cents
+import com.slack.circuit.tacos.model.Diet
 import com.slack.circuit.tacos.model.Ingredient
 import com.slack.circuit.tacos.repository.IngredientsRepository
 import kotlinx.collections.immutable.ImmutableList
-import kotlinx.collections.immutable.toImmutableSet
+import java.math.BigDecimal
 
 object FillingsOrderStep : OrderStep {
   override val number = 0
@@ -18,14 +25,14 @@ object FillingsOrderStep : OrderStep {
   sealed interface State : OrderStep.State {
     object Loading : State
     data class AvailableFillings(
+      val selected: Ingredient? = null,
       val list: ImmutableList<Ingredient>,
       val eventSink: (Event) -> Unit
     ) : State
   }
 
   sealed interface Event {
-    data class Selected(val ingredient: Ingredient) : Event
-    data class Deselected(val ingredient: Ingredient) : Event
+    data class SelectFilling(val ingredient: Ingredient) : Event
   }
 }
 
@@ -35,40 +42,98 @@ class FillingsProducerImpl(private val repository: IngredientsRepository) : Fill
     orderDetails: OrderDetails,
     eventSink: (OrderStep.Event) -> Unit
   ): FillingsOrderStep.State {
-    val selected = remember { mutableStateMapOf<Ingredient, Unit>() }
+    var selected by remember { mutableStateOf<Ingredient?>(null) }
     val ingredients by produceState<ImmutableList<Ingredient>?>(null) {
       value = repository.getFillings()
     }
 
-    return when (val data = ingredients) {
+    return when (val list = ingredients) {
       null -> {
         eventSink(OrderStep.Validation.Invalid)
         FillingsOrderStep.State.Loading
       }
-      else -> FillingsOrderStep.State.AvailableFillings(data) { event ->
+      else -> FillingsOrderStep.State.AvailableFillings(orderDetails.filling, list) { event ->
         when (event) {
-          is FillingsOrderStep.Event.Selected -> selected[event.ingredient] = Unit
-          is FillingsOrderStep.Event.Deselected -> selected.remove(event.ingredient)
+          is FillingsOrderStep.Event.SelectFilling -> {
+            selected = event.ingredient
+            eventSink(OrderStep.UpdateOrder.Filling(event.ingredient))
+            eventSink(OrderStep.Validation.Valid)
+          }
         }
-
-        updateOrder(selected.keys, eventSink)
       }
     }
-  }
-}
-
-private fun updateOrder(
-  ingredients: Set<Ingredient>,
-  eventSink: (OrderStep.Event) -> Unit
-) {
-  eventSink(OrderStep.UpdateOrder.Fillings(ingredients.toImmutableSet()))
-  when {
-    ingredients.isEmpty() -> eventSink(OrderStep.Validation.Invalid)
-    else -> eventSink(OrderStep.Validation.Valid)
   }
 }
 
 @Composable
 fun FillingsUi(state: FillingsOrderStep.State, modifier: Modifier = Modifier) {
+  when (state) {
+    is FillingsOrderStep.State.Loading -> Text("loading...", modifier)
+    is FillingsOrderStep.State.AvailableFillings -> FillingsList(state, modifier)
+  }
+}
 
+@Composable
+private fun FillingsList(
+  state: FillingsOrderStep.State.AvailableFillings,
+  modifier: Modifier = Modifier
+) {
+  val sink = state.eventSink
+  Column(modifier = modifier) {
+    state.list.forEach { ingredient ->
+      Filling(
+        ingredient = ingredient,
+        isSelected = state.selected == ingredient,
+        onSelect = { sink(FillingsOrderStep.Event.SelectFilling(ingredient)) },
+      )
+    }
+  }
+}
+
+@Composable
+private fun Filling(
+  ingredient: Ingredient,
+  isSelected: Boolean,
+  modifier: Modifier = Modifier,
+  onSelect: () -> Unit
+) {
+  Row(modifier = modifier) {
+    RadioButton(selected = isSelected, modifier = modifier, onClick = onSelect)
+    Column {
+      with(ingredient) {
+        Row {
+          Text(name)
+          DietBadge(diet)
+        }
+        AdditionalCharge(charge)
+        Spacer(charge, calories)
+        Calories(calories)
+      }
+    }
+  }
+}
+
+@Composable
+private fun DietBadge(diet: Diet, modifier: Modifier = Modifier) {
+  if (diet == Diet.NONE) return
+  Text(diet.name.lowercase(), modifier)
+}
+
+@Composable
+private fun AdditionalCharge(charge: Cents, modifier: Modifier = Modifier) {
+  if (charge <= 0) return
+  val dollarAmount = BigDecimal(charge) / BigDecimal(100)
+  Text("$$dollarAmount", modifier)
+}
+
+@Composable
+private fun Calories(calories: Int, modifier: Modifier = Modifier) {
+  if (calories <= 0) return
+  Text("$calories cals", modifier)
+}
+
+@Composable
+private fun Spacer(charge: Cents, calories: Int, modifier: Modifier = Modifier) {
+  if (charge <= 0 || calories <= 0) return
+  Text(" | ", modifier)
 }

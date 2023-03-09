64b0e7ff6ff4a42d16aae2e241fff798c0262593
Kieran Elliott <kierse@pissiphany.com>
2023-03-09 18:39:45 -0500

    Adding fake parent to help process OpenStep.Events

--- samples/tacos/src/test/kotlin/com/slack/circuit/tacos/step/TestHelpers.kt
+++ samples/tacos/src/test/kotlin/com/slack/circuit/tacos/step/TestHelpers.kt
@@ -1,4 +1,43 @@
 package com.slack.circuit.tacos.step
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import com.google.common.truth.Truth.assertThat
+import com.slack.circuit.tacos.OrderDetails
+import com.slack.circuit.tacos.model.Ingredient
+import kotlinx.collections.immutable.ImmutableSet
+import kotlinx.collections.immutable.persistentSetOf
+
 internal inline fun <reified T : Any> OrderStep.State.asInstanceOf(): T =
   (this as? T) ?: error("unable to cast $this to ${T::class.simpleName}")
+
+internal class FakeOrderStepParent(
+  filling: Ingredient? = null,
+  toppings: ImmutableSet<Ingredient> = persistentSetOf(),
+) {
+  private val validations = ArrayDeque<OrderStep.Validation>()
+  var orderDetails by mutableStateOf(
+    OrderDetails(
+      filling = filling,
+      toppings = toppings,
+    )
+  )
+    private set
+
+  fun childEvent(event: OrderStep.Event) {
+    when (event) {
+      is OrderStep.Validation -> validations.addLast(event)
+      is OrderStep.UpdateOrder.Filling ->
+        orderDetails = orderDetails.copy(filling = event.ingredient)
+      is OrderStep.UpdateOrder.Toppings ->
+        orderDetails = orderDetails.copy(toppings = event.ingredients)
+    }
+  }
+
+  fun assertValidation(expected: OrderStep.Validation) {
+    validations.isNotEmpty() || error("No unprocessed validations!")
+    assertThat(validations.removeFirst()).isEqualTo(expected)
+  }
+}
+

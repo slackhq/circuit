1cd01ac5da0967a8e233a982049ad01c2a1c4869
Zac Sweers <pandanomic@gmail.com>
2023-03-12 22:02:20 -0400

    Update baseline_profile_splitter

--- scripts/baseline_profile_splitter.sh
+++ scripts/baseline_profile_splitter.sh
@@ -13,12 +13,18 @@ removeLines() (
   mv "$tmp_file" "$all_lines"
 )
 
-awk '/circuit\/overlay/' circuit/src/androidMain/baseline-prof.txt >circuit-overlay/src/androidMain/baseline-prof.txt
-awk '/circuit\/retained/' circuit/src/androidMain/baseline-prof.txt >circuit-retained/src/androidMain/baseline-prof.txt
 # Because backstack is used in circuit itself, only look at the package before the '->' arrow
-awk '/circuit\/backstack.*->/' circuit/src/androidMain/baseline-prof.txt >backstack/src/androidMain/baseline-prof.txt
+awk '/circuit\/backstack.*->/' circuit-runtime/src/androidMain/baseline-prof.txt >backstack/src/androidMain/baseline-prof.txt
+awk '/circuit\/foundation/' circuit-runtime/src/androidMain/baseline-prof.txt >circuit-foundation/src/androidMain/baseline-prof.txt
+awk '/circuit\/overlay/' circuit-runtime/src/androidMain/baseline-prof.txt >circuit-overlay/src/androidMain/baseline-prof.txt
+awk '/circuit\/retained/' circuit-runtime/src/androidMain/baseline-prof.txt >circuit-retained/src/androidMain/baseline-prof.txt
+awk '/circuit\/runtime\/presenter/' circuit-runtime/src/androidMain/baseline-prof.txt >circuit-runtime-presenter/src/androidMain/baseline-prof.txt
+awk '/circuit\/runtime\/ui/' circuit-runtime/src/androidMain/baseline-prof.txt >circuit-runtime-ui/src/androidMain/baseline-prof.txt
 
 # Now finally remove any moved lines from the original source
-removeLines circuit-overlay/src/androidMain/baseline-prof.txt circuit/src/androidMain/baseline-prof.txt
-removeLines circuit-retained/src/androidMain/baseline-prof.txt circuit/src/androidMain/baseline-prof.txt
-removeLines backstack/src/androidMain/baseline-prof.txt circuit/src/androidMain/baseline-prof.txt
+removeLines backstack/src/androidMain/baseline-prof.txt circuit-runtime/src/androidMain/baseline-prof.txt
+removeLines circuit-foundation/src/androidMain/baseline-prof.txt circuit-runtime/src/androidMain/baseline-prof.txt
+removeLines circuit-overlay/src/androidMain/baseline-prof.txt circuit-runtime/src/androidMain/baseline-prof.txt
+removeLines circuit-retained/src/androidMain/baseline-prof.txt circuit-runtime/src/androidMain/baseline-prof.txt
+removeLines circuit-runtime-presenter/src/androidMain/baseline-prof.txt circuit-runtime/src/androidMain/baseline-prof.txt
+removeLines circuit-runtime-ui/src/androidMain/baseline-prof.txt circuit-runtime/src/androidMain/baseline-prof.txt

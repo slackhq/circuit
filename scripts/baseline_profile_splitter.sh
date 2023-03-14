e1260fe04468e47ab5bedb1da93941321793bdd0
Zac Sweers <pandanomic@gmail.com>
2023-03-14 14:40:45 -0400

    Update splitter + fix matching prefixes

--- scripts/baseline_profile_splitter.sh
+++ scripts/baseline_profile_splitter.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # This script splits the baseline profile into separate files for each circuit artifact
 # and then removes moved lines from the original core artifact.
@@ -13,13 +13,32 @@ removeLines() (
   mv "$tmp_file" "$all_lines"
 )
 
-# Because backstack is used in circuit itself, only look at the package before the '->' arrow
-awk '/circuit\/backstack.*->/' circuit-runtime/src/androidMain/baseline-prof.txt >backstack/src/androidMain/baseline-prof.txt
-awk '/circuit\/foundation/' circuit-runtime/src/androidMain/baseline-prof.txt >circuit-foundation/src/androidMain/baseline-prof.txt
-awk '/circuit\/overlay/' circuit-runtime/src/androidMain/baseline-prof.txt >circuit-overlay/src/androidMain/baseline-prof.txt
-awk '/circuit\/retained/' circuit-runtime/src/androidMain/baseline-prof.txt >circuit-retained/src/androidMain/baseline-prof.txt
-awk '/circuit\/runtime\/presenter/' circuit-runtime/src/androidMain/baseline-prof.txt >circuit-runtime-presenter/src/androidMain/baseline-prof.txt
-awk '/circuit\/runtime\/ui/' circuit-runtime/src/androidMain/baseline-prof.txt >circuit-runtime-ui/src/androidMain/baseline-prof.txt
+# Only look at the package before the '->' arrow
+# We do this by just replacing any text after an -> with nothing when doing the matching
+awk '{line = $0; sub(/->.*/, "", line); if (match(line, /circuit\/backstack.*/) > 0) print}' circuit-runtime/src/androidMain/baseline-prof.txt >backstack/src/androidMain/baseline-prof.txt
+awk '{line = $0; sub(/->.*/, "", line); if (match(line, /circuit\/foundation.*/) > 0) print}' circuit-runtime/src/androidMain/baseline-prof.txt >circuit-foundation/src/androidMain/baseline-prof.txt
+awk '{line = $0; sub(/->.*/, "", line); if (match(line, /circuit\/overlay.*/) > 0) print}' circuit-runtime/src/androidMain/baseline-prof.txt >circuit-overlay/src/androidMain/baseline-prof.txt
+awk '{line = $0; sub(/->.*/, "", line); if (match(line, /circuit\/retained.*/) > 0) print}' circuit-runtime/src/androidMain/baseline-prof.txt >circuit-retained/src/androidMain/baseline-prof.txt
+awk '{line = $0; sub(/->.*/, "", line); if (match(line, /circuit\/runtime\/presenter.*/) > 0) print}' circuit-runtime/src/androidMain/baseline-prof.txt >circuit-runtime-presenter/src/androidMain/baseline-prof.txt
+awk '{line = $0; sub(/->.*/, "", line); if (match(line, /circuit\/runtime\/ui.*/) > 0) print}' circuit-runtime/src/androidMain/baseline-prof.txt >circuit-runtime-ui/src/androidMain/baseline-prof.txt
+
+# TODO why doesn't a loop below work??
+#artifacts=(
+#  "circuit\/backstack.*:backstack"
+#  "circuit\/foundation.*:circuit-foundation"
+#  "circuit\/overlay.*:circuit-overlay"
+#  "circuit\/retained.*:circuit-retained"
+#  "circuit\/runtime\/presenter.*:circuit-runtime-presenter"
+#  "circuit\/runtime\/ui.*:circuit-runtime-ui"
+#)
+
+#for artifact in "${artifacts[@]}" ; do
+#  regex=${artifact%%:*}
+#  location=${artifact#*:}
+#  target_file="$location"/src/androidMain/baseline-prof.txt
+#  printf "Mapping '%s' to '%s'.\n" "$regex" "$target_file"
+#  awk -v regexVar="$regex" '{line = $0; sub(/->.*/, "", line); if (match(line, /regexVar/) > 0) print }' circuit-runtime/src/androidMain/baseline-prof.txt >"$target_file"
+#done
 
 # Now finally remove any moved lines from the original source
 removeLines backstack/src/androidMain/baseline-prof.txt circuit-runtime/src/androidMain/baseline-prof.txt

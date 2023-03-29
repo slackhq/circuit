792c988c1df1fb2cd0705a0ca3ffea4f9619f96e
Kieran Elliott <kierse@pissiphany.com>
2023-03-29 17:34:45 -0400

    Replaced use of Image() with Icon()

--- samples/tacos/src/main/kotlin/com/slack/circuit/tacos/OrderTacosCircuit.kt
+++ samples/tacos/src/main/kotlin/com/slack/circuit/tacos/OrderTacosCircuit.kt
@@ -4,7 +4,6 @@ package com.slack.circuit.tacos
 
 import androidx.activity.compose.BackHandler
 import androidx.annotation.StringRes
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -16,8 +15,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -30,7 +29,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.stringResource
@@ -267,17 +265,9 @@ private fun NavigationButton(
 ) {
   if (!visible) return
 
-  val tintColour =
-    when (enabled) {
-      true -> MaterialTheme.colorScheme.onSurface
-      false -> MaterialTheme.colorScheme.outline
-    }
-
   IconButton(modifier = modifier, enabled = enabled, onClick = onClick) {
-    Image(
-      modifier = Modifier,
+    Icon(
       painter = rememberVectorPainter(image = direction.icon),
-      colorFilter = ColorFilter.tint(tintColour),
       contentDescription = stringResource(direction.descriptionResId),
     )
   }

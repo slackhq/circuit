29f7035dc76724467a0eeb455d2d20ef0a108eb3
Zac Sweers <pandanomic@gmail.com>
2023-04-23 12:15:43 -0400

    Consolidate counter example to single kmm app instead of separate android/desktop/ios

--- samples/counter/build.gradle.kts
+++ samples/counter/build.gradle.kts
@@ -1,10 +1,23 @@
 // Copyright (C) 2022 Slack Technologies, LLC
 // SPDX-License-Identifier: Apache-2.0
-plugins { kotlin("multiplatform") }
+plugins {
+  kotlin("multiplatform")
+  id("com.android.library")
+}
 
 kotlin {
   // region KMP Targets
+  android { publishLibraryVariants("release") }
   jvm()
+  listOf(
+    iosX64(),
+    iosArm64(),
+    iosSimulatorArm64()
+  ).forEach {
+    it.binaries.framework {
+      baseName = "counter"
+    }
+  }
   // endregion
 
   sourceSets {
@@ -17,3 +30,7 @@ kotlin {
     maybeCreate("commonTest").apply { dependencies { implementation(libs.kotlin.test) } }
   }
 }
+
+android { namespace = "com.slack.circuit.sample.counter" }
+
+androidComponents { beforeVariants { variant -> variant.enableAndroidTest = false } }

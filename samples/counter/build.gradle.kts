144996da8dc01bc889fb892aa0f4a369766a9221
Zac Sweers <pandanomic@gmail.com>
2023-04-23 14:18:10 -0400

    More iOS build tinkering

--- samples/counter/build.gradle.kts
+++ samples/counter/build.gradle.kts
@@ -2,15 +2,36 @@
 // SPDX-License-Identifier: Apache-2.0
 plugins {
   kotlin("multiplatform")
+  kotlin("native.cocoapods")
   id("com.android.library")
 }
+
+version = "1.0.0-SNAPSHOT"
 
 kotlin {
   // region KMP Targets
   android { publishLibraryVariants("release") }
   jvm()
-  listOf(iosX64(), iosArm64(), iosSimulatorArm64()).forEach {
-    it.binaries.framework { baseName = "counter" }
+  ios()
+  iosSimulatorArm64()
+  // TODO regular frameworks are not yet supported
+  //  listOf(iosX64(), iosArm64(), iosSimulatorArm64()).forEach {
+  //    it.binaries.framework {
+  //      baseName = "counter"
+  //      isStatic = true
+  //    }
+  //  }
+
+  cocoapods {
+    summary = "Counter presenter implementation"
+    homepage = "None"
+    ios.deploymentTarget = "14.1"
+    podfile = project.file("apps/Podfile")
+    framework {
+      baseName = "counter"
+      isStatic = true
+    }
+    extraSpecAttributes["resources"] = "['src/commonMain/resources/**', 'src/iosMain/resources/**']"
   }
   // endregion
 

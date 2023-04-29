4ab296a969cbbab12c94a4d666484b96a87f4e28
Zac Sweers <pandanomic@gmail.com>
2023-04-29 15:12:53 -0400

    Suppressions

--- samples/counter/src/androidMain/kotlin/com/slack/circuit/sample/counter/util/Dispatchers.android.kt
+++ samples/counter/src/androidMain/kotlin/com/slack/circuit/sample/counter/util/Dispatchers.android.kt
@@ -3,5 +3,6 @@ package com.slack.circuit.sample.counter.util;
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
+@Suppress("EXTENSION_SHADOWED_BY_MEMBER")
 actual val Dispatchers.IO: CoroutineDispatcher
   get() = Dispatchers.IO
--- samples/counter/src/commonMain/kotlin/com/slack/circuit/sample/counter/util/Dispatchers.kt
+++ samples/counter/src/commonMain/kotlin/com/slack/circuit/sample/counter/util/Dispatchers.kt
@@ -3,4 +3,6 @@ package com.slack.circuit.sample.counter.util
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
+// Amazing that we have to suppress the lint on the suppression
+@Suppress("EXTENSION_SHADOWED_BY_MEMBER", "KotlinRedundantDiagnosticSuppress")
 expect val Dispatchers.IO: CoroutineDispatcher

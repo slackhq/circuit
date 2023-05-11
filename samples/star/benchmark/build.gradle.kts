10adde673c3dba11bd56c40d28b1435132fea0b5
Zac Sweers <pandanomic@gmail.com>
2023-05-11 01:40:54 -0400

    Add new baseline profile plugin

--- samples/star/benchmark/build.gradle.kts
+++ samples/star/benchmark/build.gradle.kts
@@ -3,6 +3,7 @@
 plugins {
   id("com.android.test")
   kotlin("android")
+  alias(libs.plugins.baselineprofile)
 }
 
 android {
@@ -23,8 +24,26 @@ android {
     }
   }
 
+  testOptions.managedDevices.devices {
+    create<com.android.build.api.dsl.ManagedVirtualDevice>("pixel6Api31") {
+      device = "Pixel 6"
+      apiLevel = 31
+      systemImageSource = "aosp"
+    }
+  }
+
   targetProjectPath = ":samples:star:apk"
   experimentalProperties["android.experimental.self-instrumenting"] = true
+}
+
+baselineProfile {
+  // This specifies the managed devices to use that you run the tests on. The default
+  // is none.
+  managedDevices += "pixel6Api31"
+
+  // This enables using connected devices to generate profiles. The default is true.
+  // When using connected devices, they must be rooted or API 33 and higher.
+  useConnectedDevices = false
 }
 
 dependencies {

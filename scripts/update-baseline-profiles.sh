132b1f291ae29469fd669a59082867ac5c71a57d
Zac Sweers <pandanomic@gmail.com>
2023-05-15 15:02:36 -0400

    Try fixing syntax

--- scripts/update-baseline-profiles.sh
+++ scripts/update-baseline-profiles.sh
@@ -4,7 +4,7 @@ set -uo pipefail
 # If on CI, add indirect swiftshader arg
 gpu_arg=""
 if [[ "$CI" == "true" ]]; then
-  gpu_arg="-Pandroid.testoptions.manageddevices.emulator.gpu=\"swiftshader_indirect\""
+  gpu_arg="-Pandroid.testoptions.manageddevices.emulator.gpu=swiftshader_indirect"
 fi
 
 ./gradlew cleanManagedDevices --unused-only &&

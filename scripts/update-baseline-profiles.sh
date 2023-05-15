3a05a002b41ecf641cd026585087257db089d386
Zac Sweers <pandanomic@gmail.com>
2023-05-15 12:30:44 -0400

    More debugging

--- scripts/update-baseline-profiles.sh
+++ scripts/update-baseline-profiles.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 set -uo pipefail
 
-./gradlew generateReleaseBaselineProfile -Pandroid.testInstrumentationRunnerArguments.androidx.benchmark.enabledRules=baselineprofile --quiet
+# If on CI, add indirect swiftshader arg
+gpu_arg=""
+if [[ "$CI" == "true" ]]; then
+  gpu_arg="-Pandroid.testoptions.manageddevices.emulator.gpu=\"swiftshader_indirect\""
+fi
+
+./gradlew cleanManagedDevices --unused-only &&
+    ./gradlew generateReleaseBaselineProfile \
+    -Pandroid.testInstrumentationRunnerArguments.androidx.benchmark.enabledRules=baselineprofile \
+    -Dorg.gradle.workers.max=1 \
+    "${gpu_arg}" \
+    --quiet

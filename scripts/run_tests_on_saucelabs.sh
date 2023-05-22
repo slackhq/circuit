9b33eb386d62475bb6fd172950f6a6f767f187ab
Jason Stewart <jason@slack-corp.com>
2023-05-22 12:23:31 -0700

    add ./

--- scripts/run_tests_on_saucelabs.sh
+++ scripts/run_tests_on_saucelabs.sh
@@ -7,5 +7,5 @@ sh -c 'curl -L https://saucelabs.github.io/saucectl/install | bash -s -- -b ./'
 chmod +x saucectl
 
 echo "[ INFO ] Executing Tests on Sauce Labs"
-saucectl configure --username sso-saleforce-j.stewart --accessKey ${SAUCELABS_TOKEN}
-saucectl run -c scripts/sauce_labs_config.yml
+./saucectl configure --username sso-saleforce-j.stewart --accessKey ${SAUCELABS_TOKEN}
+./saucectl run -c scripts/sauce_labs_config.yml

204bb7acf18d1a3860d244f7b316f176e7672acd
Zac Sweers <pandanomic@gmail.com>
2023-07-28 14:18:34 -0800

    Make migration easier with deprecation replacements

---
+++ circuit-foundation/src/commonMain/kotlin/com/slack/circuit/foundation/CircuitConfig.kt
@@ -0,0 +1,21 @@
+// Copyright (C) 2022 Slack Technologies, LLC
+// SPDX-License-Identifier: Apache-2.0
+package com.slack.circuit.foundation
+
+import com.slack.circuit.runtime.CircuitContext
+
+/*
+ * Deprecated type markers for ease of migration.
+ */
+
+@Deprecated("Use Circuit instead", ReplaceWith("Circuit", "com.slack.circuit.foundation.Circuit"))
+public typealias CircuitConfig = Circuit
+
+@Deprecated(
+    "Use CircuitContext.circuit instead",
+    ReplaceWith("circuit", "com.slack.circuit.foundation.circuit"))
+public var CircuitContext.config: Circuit
+  get() = circuit
+  set(value) {
+    circuit = value
+  }

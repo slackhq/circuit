95dfc5178ae1869847f95b7f17ff7afec4d8a3de
Zac Sweers <pandanomic@gmail.com>
2023-08-12 18:40:03 -0400

    Update bottomsheet impl with latest from catchup

--- circuitx/overlays/src/androidMain/kotlin/com/slack/circuitx/overlays/BottomSheetOverlay.kt
+++ circuitx/overlays/src/androidMain/kotlin/com/slack/circuitx/overlays/BottomSheetOverlay.kt
@@ -2,12 +2,19 @@
 // SPDX-License-Identifier: Apache-2.0
 package com.slack.circuitx.overlays
 
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetValue
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.unit.dp
 import com.slack.circuit.foundation.internal.BackHandler
 import com.slack.circuit.overlay.Overlay
@@ -18,43 +25,72 @@ import kotlinx.coroutines.launch
 @OptIn(ExperimentalMaterial3Api::class)
 public class BottomSheetOverlay<Model : Any, Result : Any>(
   private val model: Model,
-  private val onDismiss: () -> Result,
+  private val dismissOnTapOutside: Boolean = true,
+  private val onDismiss: (() -> Result)? = null,
   private val content: @Composable (Model, OverlayNavigator<Result>) -> Unit,
 ) : Overlay<Result> {
   @Composable
   override fun Content(navigator: OverlayNavigator<Result>) {
-    val sheetState = rememberModalBottomSheetState()
-
-    val coroutineScope = rememberStableCoroutineScope()
-    BackHandler(enabled = sheetState.isVisible) {
-      coroutineScope
-        .launch { sheetState.hide() }
-        .invokeOnCompletion {
-          if (!sheetState.isVisible) {
-            navigator.finish(onDismiss())
+    var hasShown by remember { mutableStateOf(false) }
+    val sheetState =
+      rememberModalBottomSheetState(
+        confirmValueChange = { newValue ->
+          if (hasShown && newValue == SheetValue.Hidden) {
+            dismissOnTapOutside
+          } else {
+            true
           }
         }
-    }
+      )
 
+    var pendingResult by remember { mutableStateOf<Result?>(null) }
     ModalBottomSheet(
       content = {
+        val coroutineScope = rememberStableCoroutineScope()
+        BackHandler(enabled = sheetState.isVisible) {
+          coroutineScope
+            .launch { sheetState.hide() }
+            .invokeOnCompletion {
+              if (!sheetState.isVisible) {
+                navigator.finish(onDismiss!!.invoke())
+              }
+            }
+        }
         // Delay setting the result until we've finished dismissing
         content(model) { result ->
           // This is the OverlayNavigator.finish() callback
           coroutineScope.launch {
-            try {
-              sheetState.hide()
-            } finally {
-              navigator.finish(result)
-            }
+            pendingResult = result
+            sheetState.hide()
           }
         }
       },
       sheetState = sheetState,
       shape = RoundedCornerShape(32.dp),
-      onDismissRequest = { navigator.finish(onDismiss()) },
+      // Go edge-to-edge
+      windowInsets = WindowInsets(0, 0, 0, 0),
+      onDismissRequest = {
+        // Only possible if dismissOnTapOutside is false
+        check(dismissOnTapOutside)
+        navigator.finish(onDismiss!!.invoke())
+      },
     )
 
-    LaunchedEffect(Unit) { sheetState.show() }
+    LaunchedEffect(model, onDismiss) {
+      snapshotFlow { sheetState.currentValue }
+        .collect { newValue ->
+          if (hasShown && newValue == SheetValue.Hidden) {
+            // This is apparently as close as we can get to an "onDismiss" callback, which
+            // unfortunately has no animation
+            val result = pendingResult ?: onDismiss?.invoke() ?: error("no result!")
+            navigator.finish(result)
+          }
+        }
+    }
+    LaunchedEffect(model, onDismiss) {
+      // TODO why doesn't this ever hit if it's after show()
+      hasShown = true
+      sheetState.show()
+    }
   }
 }

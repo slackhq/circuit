5b1a4a93a62df3cf186caf86b3d4d3777068ed11
Zac Sweers <pandanomic@gmail.com>
2023-09-20 15:28:39 -0400

    Build file cleanups

--- circuit-codegen-annotations/build.gradle.kts
+++ circuit-codegen-annotations/build.gradle.kts
@@ -1,7 +1,5 @@
 // Copyright (C) 2022 Slack Technologies, LLC
 // SPDX-License-Identifier: Apache-2.0
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-
 plugins {
   alias(libs.plugins.agp.library)
   alias(libs.plugins.kotlin.multiplatform)
@@ -15,7 +13,7 @@ kotlin {
   // Anvil/Dagger does not support iOS targets
   // endregion
 
-  @OptIn(ExperimentalKotlinGradlePluginApi::class) targetHierarchy.default()
+  applyDefaultHierarchyTemplate()
 
   sourceSets {
     commonMain {

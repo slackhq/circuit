f9b1fc94268bfa22a3f27604a07560b79b711c43
Zac Sweers <pandanomic@gmail.com>
2023-10-04 14:03:58 -0400

    Try adding suppression

--- circuit-test/src/iosMain/kotlin/com/slack/circuit/test/TestEventSink.ios.kt
+++ circuit-test/src/iosMain/kotlin/com/slack/circuit/test/TestEventSink.ios.kt
@@ -2,4 +2,5 @@
 // SPDX-License-Identifier: Apache-2.0
 package com.slack.circuit.test
 
+@Suppress("ACTUAL_TYPE_ALIAS_TO_CLASS_WITH_DECLARATION_SITE_VARIANCE", "INCOMPATIBLE_MATCHING")
 public actual sealed interface BaseTestEventSinkType<UiEvent> : (UiEvent) -> Unit

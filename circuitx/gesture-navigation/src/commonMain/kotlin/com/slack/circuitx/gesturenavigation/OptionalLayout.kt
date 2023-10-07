364cd27ccd47dd34bc8042b97e9dc3545f9663f0
Zac Sweers <pandanomic@gmail.com>
2023-10-07 13:58:56 -0400

    Fix typo

--- circuitx/gesture-navigation/src/commonMain/kotlin/com/slack/circuitx/gesturenavigation/OptionalLayout.kt
+++ circuitx/gesture-navigation/src/commonMain/kotlin/com/slack/circuitx/gesturenavigation/OptionalLayout.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.layout.layout
  * A composable which optionally lays out the given [content], depending on what [shouldLayout]
  * returns, whilst still keeping [content] attached to composition.
  *
- * This is useful when used in conjuction with `movableContentOf`, as that will destroy the
+ * This is useful when used in conjunction with `movableContentOf`, as that will destroy the
  * content's state 'very soon' after the content is detached from composition. By using this
  * composable, the content can remain attached to composition, but can avoid being measured, laid
  * out or drawn, allowing us to reduce wasted work.

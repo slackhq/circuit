b2a95f492af03f7daeb1e97228d4ccd6981b2e32
Zac Sweers <pandanomic@gmail.com>
2023-10-07 16:49:44 -0400

    Switch out deps

--- samples/star/src/main/kotlin/com/slack/circuit/star/data/TokenStorageModule.kt
+++ samples/star/src/main/kotlin/com/slack/circuit/star/data/TokenStorageModule.kt
@@ -8,10 +8,12 @@ import com.slack.circuit.star.datastore.createStorage
 import com.slack.circuit.star.di.AppScope
 import com.slack.circuit.star.di.ApplicationContext
 import com.squareup.anvil.annotations.ContributesTo
+import dagger.Module
 import dagger.Provides
 import okio.Path.Companion.toOkioPath
 
 @ContributesTo(AppScope::class)
+@Module
 object TokenStorageModule {
   private const val TOKEN_STORAGE_FILE_NAME = "TokenManager"
 

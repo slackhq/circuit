5f3fe7310d91bc4e051549bff1fa4bb3be5435d2
Zac Sweers <pandanomic@gmail.com>
2023-10-24 13:05:34 -0400

    Restore more

--- circuit-test/src/jvmMain/kotlin/com/slack/circuit/test/TestEventSink.jvm.kt
+++ circuit-test/src/jvmMain/kotlin/com/slack/circuit/test/TestEventSink.jvm.kt
@@ -2,8 +2,4 @@
 // SPDX-License-Identifier: Apache-2.0
 package com.slack.circuit.test
 
-@OptIn(ExperimentalMultiplatform::class)
-@AllowDifferentMembersInActual
-// https://youtrack.jetbrains.com/issue/KT-21846
-@Suppress("ACTUAL_TYPE_ALIAS_TO_CLASS_WITH_DECLARATION_SITE_VARIANCE", "INCOMPATIBLE_MATCHING")
 public actual sealed interface BaseTestEventSinkType<UiEvent> : (UiEvent) -> Unit

4be4367551c59af464229c9da97910497e1179be
Zac Sweers <pandanomic@gmail.com>
2023-12-21 15:32:09 -0500

    Add UI tests + poko sample

--- samples/star/src/test/kotlin/com/slack/circuit/star/stateprototyping/CounterCircuit.kt
+++ samples/star/src/test/kotlin/com/slack/circuit/star/stateprototyping/CounterCircuit.kt
@@ -2,19 +2,38 @@
 // SPDX-License-Identifier: Apache-2.0
 package com.slack.circuit.star.stateprototyping
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.assertTextContains
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.printToString
 import com.google.common.truth.Truth.assertThat
 import com.slack.circuit.runtime.CircuitUiEvent
 import com.slack.circuit.runtime.CircuitUiState
 import com.slack.circuit.runtime.presenter.Presenter
+import com.slack.circuit.star.stateprototyping.CounterState.AsAbstractClassWithEventSink
+import com.slack.circuit.star.stateprototyping.CounterState.AsDataClass
+import com.slack.circuit.star.stateprototyping.CounterState.AsInterface
+import com.slack.circuit.star.stateprototyping.CounterState.AsInterfaceWithEventSink
+import com.slack.circuit.star.stateprototyping.CounterState.AsPokoClass
 import com.slack.circuit.test.test
 import kotlinx.coroutines.test.runTest
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.ParameterizedRobolectricTestRunner
 import org.robolectric.RobolectricTestRunner
 
 sealed interface CounterState : CircuitUiState {
@@ -22,9 +41,10 @@ sealed interface CounterState : CircuitUiState {
 
   /** An interface-only approach where all the events are defined as functions on the interface. */
   interface AsInterface : CounterState {
-    fun increment()
+    // TODO should these have empty defaults for easier testing?
+    fun increment() {}
 
-    fun decrement()
+    fun decrement() {}
   }
 
   /**
@@ -38,13 +58,20 @@ sealed interface CounterState : CircuitUiState {
     eventSink: (Event) -> Unit = {},
   ) : CounterState, EventSink<Event> by eventSink(eventSink)
 
-  /** As an abstract class with a helper constructor param for funneling events and properties. */
-  abstract class AsAbstractClassWithEventSinkAndProperties(
-    final override val count: Int,
+  /**
+   * As a Poko class with a helper constructor param for funneling events and properties.
+   *
+   * Note that Poko isn't actually enabled on the project, but would use a class like this.
+   *
+   * See https://github.com/drewhamilton/Poko
+   */
+  // @Poko
+  class AsPokoClass(
+    override val count: Int,
     eventSink: (Event) -> Unit = {},
   ) : CounterState, EventSink<Event> by eventSink(eventSink)
 
-  /** A standard data class implementation of a canonical Circuit state. */
+  /** As a standard data class implementation of a canonical Circuit state. */
   data class AsDataClass(
     override val count: Int,
     val eventSink: (Event) -> Unit = {},
@@ -57,11 +84,11 @@ sealed interface Event : CircuitUiEvent {
   data object Decrement : Event
 }
 
-class AsInterfacePresenter : Presenter<CounterState.AsInterface> {
+class AsInterfacePresenter : Presenter<AsInterface> {
   @Composable
-  override fun present(): CounterState.AsInterface {
+  override fun present(): AsInterface {
     return remember {
-      object : CounterState.AsInterface {
+      object : AsInterface {
         override var count: Int by mutableIntStateOf(0)
           private set
 
@@ -77,11 +104,11 @@ class AsInterfacePresenter : Presenter<CounterState.AsInterface> {
   }
 }
 
-class AsInterfaceWithEventSinkPresenter : Presenter<CounterState.AsInterfaceWithEventSink> {
+class AsInterfaceWithEventSinkPresenter : Presenter<AsInterfaceWithEventSink> {
   @Composable
-  override fun present(): CounterState.AsInterfaceWithEventSink {
+  override fun present(): AsInterfaceWithEventSink {
     return remember {
-      object : CounterState.AsInterfaceWithEventSink {
+      object : AsInterfaceWithEventSink {
         override var count: Int by mutableIntStateOf(0)
           private set
 
@@ -100,11 +127,11 @@ class AsInterfaceWithEventSinkPresenter : Presenter<CounterState.AsInterfaceWith
   }
 }
 
-class AsAbstractClassWithEventSinkPresenter : Presenter<CounterState.AsAbstractClassWithEventSink> {
+class AsAbstractClassWithEventSinkPresenter : Presenter<AsAbstractClassWithEventSink> {
   @Composable
-  override fun present(): CounterState.AsAbstractClassWithEventSink {
+  override fun present(): AsAbstractClassWithEventSink {
     return remember {
-      object : CounterState.AsAbstractClassWithEventSink() {
+      object : AsAbstractClassWithEventSink() {
         override var count: Int by mutableIntStateOf(0)
           private set
 
@@ -123,32 +150,29 @@ class AsAbstractClassWithEventSinkPresenter : Presenter<CounterState.AsAbstractC
   }
 }
 
-class AsAbstractClassWithEventSinkAndPropertiesPresenter :
-  Presenter<CounterState.AsAbstractClassWithEventSinkAndProperties> {
+class AsPokoClassPresenter : Presenter<AsPokoClass> {
   @Composable
-  override fun present(): CounterState.AsAbstractClassWithEventSinkAndProperties {
+  override fun present(): AsPokoClass {
     var count by remember { mutableIntStateOf(0) }
     // TODO could this be remembered too? Does the State get captured and thus keep it live?
-    return object : CounterState.AsAbstractClassWithEventSinkAndProperties(count) {
-      override fun send(event: Event) {
-        when (event) {
-          is Event.Increment -> {
-            count++
-          }
-          is Event.Decrement -> {
-            count--
-          }
+    return AsPokoClass(count) { event ->
+      when (event) {
+        is Event.Increment -> {
+          count++
+        }
+        is Event.Decrement -> {
+          count--
         }
       }
     }
   }
 }
 
-class AsDataClassPresenter : Presenter<CounterState.AsDataClass> {
+class AsDataClassPresenter : Presenter<AsDataClass> {
   @Composable
-  override fun present(): CounterState.AsDataClass {
+  override fun present(): AsDataClass {
     var count by remember { mutableIntStateOf(0) }
-    return CounterState.AsDataClass(
+    return AsDataClass(
       count = count,
       eventSink = { event ->
         when (event) {
@@ -164,7 +188,8 @@ class AsDataClassPresenter : Presenter<CounterState.AsDataClass> {
   }
 }
 
-@RunWith(RobolectricTestRunner::class) // Necessary because ComposerImpl touches android.os.Trace
+// Robolectric is necessary because ComposerImpl touches android.os.Trace
+@RunWith(RobolectricTestRunner::class)
 class Tests {
   @Test
   fun asInterface() = runTest {
@@ -209,8 +234,8 @@ class Tests {
   }
 
   @Test
-  fun asAbstractClassWithEventSinkAndProperties() = runTest {
-    val presenter = AsAbstractClassWithEventSinkAndPropertiesPresenter()
+  fun asPokoClass() = runTest {
+    val presenter = AsPokoClassPresenter()
     presenter.test {
       val state = awaitItem()
       assertThat(state.count).isEqualTo(0)
@@ -235,6 +260,131 @@ class Tests {
       state.eventSink(Event.Decrement)
       val state3 = awaitItem()
       assertThat(state3.count).isEqualTo(0)
+    }
+  }
+}
+
+abstract class BaseUiTest {
+
+  @get:Rule val composeTestRule = createComposeRule()
+
+  @Composable
+  fun Counter(state: CounterState, modifier: Modifier = Modifier) {
+    Column(modifier) {
+      Button(
+        onClick = {
+          when (state) {
+            is AsInterface -> state.increment()
+            is AsInterfaceWithEventSink -> state.send(Event.Increment)
+            is AsAbstractClassWithEventSink -> state.send(Event.Increment)
+            is AsPokoClass -> state.send(Event.Increment)
+            is AsDataClass -> state.eventSink(Event.Increment)
+          }
+        }
+      ) {
+        Text(text = "Increment")
+      }
+      Text(text = "Count: ${state.count}", modifier = Modifier.testTag("COUNT"))
+      Button(
+        onClick = {
+          when (state) {
+            is AsInterface -> state.decrement()
+            is AsInterfaceWithEventSink -> state.send(Event.Decrement)
+            is AsAbstractClassWithEventSink -> state.send(Event.Decrement)
+            is AsPokoClass -> state.send(Event.Decrement)
+            is AsDataClass -> state.eventSink(Event.Decrement)
+          }
+        }
+      ) {
+        Text(text = "Decrement")
+      }
+    }
+  }
+}
+
+/**
+ * Tests that demonstrate what it's like standing up new functional [Counter] UIs with presenters.
+ */
+@RunWith(ParameterizedRobolectricTestRunner::class)
+class FunctionalUiTest(private val presenter: Presenter<CounterState>) : BaseUiTest() {
+  companion object {
+    @JvmStatic
+    @ParameterizedRobolectricTestRunner.Parameters(name = "{0}")
+    fun data(): List<Array<Any>> {
+      return listOf(
+        arrayOf(AsInterfacePresenter()),
+        arrayOf(AsInterfaceWithEventSinkPresenter()),
+        arrayOf(AsAbstractClassWithEventSinkPresenter()),
+        arrayOf(AsPokoClassPresenter()),
+        arrayOf(AsDataClassPresenter()),
+      )
+    }
+  }
+
+  @Test
+  fun functionalTest() {
+    composeTestRule.run {
+      setContent {
+        val state = presenter.present()
+        Counter(state)
+      }
+
+      println(onRoot().printToString())
+
+      onNodeWithText("Count").assertTextContains("0")
+      onNodeWithText("Increment").performClick()
+      onNodeWithText("Count").assertTextContains("1")
+      onNodeWithText("Decrement").performClick()
+      onNodeWithText("Count").assertTextContains("0")
+    }
+  }
+}
+
+/**
+ * Tests that demonstrate what it's like standing up new [CounterState] instances manually. This
+ * reflects how simple UI tests without a presenter, previews, or snapshot tests would work.
+ */
+@RunWith(ParameterizedRobolectricTestRunner::class)
+class StaticUiTest(val state: (count: Int) -> CounterState) : BaseUiTest() {
+  companion object {
+    @Suppress("DELEGATED_MEMBER_HIDES_SUPERTYPE_OVERRIDE")
+    @JvmStatic
+    @ParameterizedRobolectricTestRunner.Parameters(name = "{0}")
+    fun data(): List<Array<Any>> {
+      return listOf(
+        arrayOf({ count: Int ->
+          object : AsInterface {
+            override val count = count
+          }
+        }),
+        arrayOf({ count: Int ->
+          object : AsInterfaceWithEventSink, EventSink<Event> by fakeSink() {
+            override val count = count
+          }
+        }),
+        arrayOf({ count: Int ->
+          object : AsAbstractClassWithEventSink(), EventSink<Event> by fakeSink() {
+            override val count = count
+          }
+        }),
+        arrayOf(::AsPokoClass),
+        // Standard data class state. Simplest to stand up
+        arrayOf(::AsDataClass),
+      )
+    }
+  }
+
+  @Test
+  fun simpleTest() {
+    composeTestRule.run {
+      var count by mutableStateOf(state(0))
+      setContent { Counter(count) }
+
+      println(onRoot().printToString())
+
+      onNodeWithText("Count").assertTextContains("0")
+      count = state(1)
+      onNodeWithText("Count").assertTextContains("1")
     }
   }
 }
--- samples/star/src/test/kotlin/com/slack/circuit/star/stateprototyping/EventSink.kt
+++ samples/star/src/test/kotlin/com/slack/circuit/star/stateprototyping/EventSink.kt
@@ -10,6 +10,13 @@ interface EventSink<UiEvent : CircuitUiEvent> {
   fun send(event: UiEvent)
 }
 
+fun <UiEvent : CircuitUiEvent> fakeSink(): EventSink<UiEvent> =
+  object : EventSink<UiEvent> {
+    override fun send(event: UiEvent) {
+      // Do nothing
+    }
+  }
+
 inline fun <UiEvent : CircuitUiEvent> eventSink(
   crossinline body: (UiEvent) -> Unit
 ): EventSink<UiEvent> =

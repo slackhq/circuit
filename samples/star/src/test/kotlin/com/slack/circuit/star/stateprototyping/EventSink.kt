6b9319fca448dc7d8b41981d7ba4351a0f63e674
Zac Sweers <pandanomic@gmail.com>
2023-12-22 01:13:02 -0500

    Optimize eventSink

--- samples/star/src/test/kotlin/com/slack/circuit/star/stateprototyping/EventSink.kt
+++ samples/star/src/test/kotlin/com/slack/circuit/star/stateprototyping/EventSink.kt
@@ -17,11 +17,25 @@ fun <UiEvent : CircuitUiEvent> fakeSink(): EventSink<UiEvent> =
     }
   }
 
+/**
+ * Creates an [EventSink] that calls the given [body] when [EventSink.send] is called.
+ *
+ * Note this inline function + [InlineEventSink] return type are a bit of bytecode trickery to avoid
+ * creating a new class for every lambda passed to this function. The end result should be that the
+ * lambda is inlined directly to the field in the implementing class and the inlined
+ * [EventSink.send] method impl is inlined directly as well to call it.
+ */
+@Suppress("NOTHING_TO_INLINE")
 inline fun <UiEvent : CircuitUiEvent> eventSink(
-  crossinline body: (UiEvent) -> Unit
-): EventSink<UiEvent> =
-  object : EventSink<UiEvent> {
-    override fun send(event: UiEvent) {
-      body(event)
-    }
+  noinline body: (UiEvent) -> Unit
+): InlineEventSink<UiEvent> = InlineEventSink(body)
+
+/** @see eventSink */
+@JvmInline
+value class InlineEventSink<UiEvent : CircuitUiEvent>
+@PublishedApi
+internal constructor(private val body: (UiEvent) -> Unit) : EventSink<UiEvent> {
+  override fun send(event: UiEvent) {
+    body(event)
   }
+}

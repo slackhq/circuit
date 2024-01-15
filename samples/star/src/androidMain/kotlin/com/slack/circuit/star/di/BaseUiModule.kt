7d6af106e4ba56cf5b7faa992624a86038c3bbd9
Zac Sweers <pandanomic@gmail.com>
2024-01-14 22:02:35 -0500

    Consolidate CoilModule

--- samples/star/src/androidMain/kotlin/com/slack/circuit/star/di/BaseUiModule.kt
+++ samples/star/src/androidMain/kotlin/com/slack/circuit/star/di/BaseUiModule.kt
@@ -3,30 +3,12 @@
 package com.slack.circuit.star.di
 
 import android.app.Activity
-import android.content.Context
-import coil3.ImageLoader
-import coil3.network.NetworkFetcher
 import com.squareup.anvil.annotations.ContributesTo
-import com.squareup.anvil.annotations.optional.SingleIn
 import dagger.Module
-import dagger.Provides
 import dagger.multibindings.Multibinds
-import io.ktor.client.HttpClient
 
 @ContributesTo(AppScope::class)
 @Module
 interface BaseUiModule {
   @Multibinds fun provideActivityProviders(): Map<Class<out Activity>, Activity>
-
-  companion object {
-    @SingleIn(AppScope::class)
-    @Provides
-    fun provideImageLoader(
-      @ApplicationContext context: Context,
-      httpClient: dagger.Lazy<HttpClient>
-    ): ImageLoader =
-      ImageLoader.Builder(context)
-        .components { add(NetworkFetcher.Factory(lazy { httpClient.get() })) }
-        .build()
-  }
 }
--- samples/star/src/jvmMain/kotlin/com/slack/circuit/star/di/BaseUiModule.kt
+++ samples/star/src/jvmMain/kotlin/com/slack/circuit/star/di/BaseUiModule.kt
@@ -2,22 +2,17 @@
 // SPDX-License-Identifier: Apache-2.0
 package com.slack.circuit.star.di
 
-import coil3.ImageLoader
 import coil3.PlatformContext
-import coil3.network.NetworkFetcher
 import com.squareup.anvil.annotations.ContributesTo
 import com.squareup.anvil.annotations.optional.SingleIn
 import dagger.Module
 import dagger.Provides
-import io.ktor.client.HttpClient
 
 @ContributesTo(AppScope::class)
 @Module
 object BaseUiModule {
+  @ApplicationContext
   @SingleIn(AppScope::class)
   @Provides
-  fun provideImageLoader(httpClient: dagger.Lazy<HttpClient>): ImageLoader =
-    ImageLoader.Builder(PlatformContext.INSTANCE)
-      .components { add(NetworkFetcher.Factory(lazy { httpClient.get() })) }
-      .build()
+  fun provideAppContext(): PlatformContext = PlatformContext.INSTANCE
 }

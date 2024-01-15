2012711ad50095e9cbfb8781acb6be9a1499fe8c
Zac Sweers <pandanomic@gmail.com>
2024-01-15 14:39:35 -0500

    Inline it

--- circuit-overlay/src/commonMain/kotlin/com/slack/circuit/overlay/OverlayEffect.kt
+++ circuit-overlay/src/commonMain/kotlin/com/slack/circuit/overlay/OverlayEffect.kt
@@ -2,13 +2,12 @@ package com.slack.circuit.overlay
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.key
 import com.slack.circuit.overlay.OverlayState.UNAVAILABLE
 import kotlinx.coroutines.CoroutineScope
 
 /**
- * Calls the given [block] with the current [OverlayHost] if overlays are available. If overlays
- * are [unavailable][OverlayState.UNAVAILABLE], the [fallback] composable will be called instead.
+ * Calls the given [block] with the current [OverlayHost] if overlays are available. If overlays are
+ * [unavailable][OverlayState.UNAVAILABLE], the [fallback] composable will be called instead.
  *
  * The [block] is executed inside a [LaunchedEffect] with the given [keys] and passes the current
  * [OverlayHost] as the only parameter. Callers should call [OverlayHost.show] within this block.
@@ -25,10 +24,6 @@ public fun OverlayEffect(
     fallback?.invoke()
   } else {
     val host = LocalOverlayHost.current
-    key(host) {
-      LaunchedEffect(
-        keys = keys,
-      ) { block(host) }
-    }
+    LaunchedEffect(host, *keys) { block(host) }
   }
 }

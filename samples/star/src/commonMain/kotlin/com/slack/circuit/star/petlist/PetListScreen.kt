aaf43170df9863653703b285e0ec125276d34f5b
Zac Sweers <pandanomic@gmail.com>
2024-01-27 16:32:29 -0500

    Add composite icon button for demo

--- samples/star/src/commonMain/kotlin/com/slack/circuit/star/petlist/PetListScreen.kt
+++ samples/star/src/commonMain/kotlin/com/slack/circuit/star/petlist/PetListScreen.kt
@@ -3,7 +3,9 @@
 package com.slack.circuit.star.petlist
 
 import androidx.compose.animation.core.AnimationConstants
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Arrangement.spacedBy
 import androidx.compose.foundation.layout.Box
@@ -17,6 +19,7 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
@@ -33,12 +36,12 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
@@ -56,10 +59,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateMap
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.capitalize
 import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.unit.dp
@@ -261,10 +266,9 @@ internal fun PetList(state: State, modifier: Modifier = Modifier) {
         scrollBehavior = scrollBehavior,
         actions = {
           if (state is Success) {
-            IconButton(
-              // TODO can we add onClick _and_ onLongClick so we can support both
-              //  screen and overlay impls? Modifier.combinedClickable doesn't work
-              onClick = { state.eventSink(GoToFiltersScreen) }
+            CompositeIconButton(
+              onClick = { state.eventSink(ShowFiltersOverlay) },
+              onLongClick = { state.eventSink(GoToFiltersScreen) },
             ) {
               Icon(
                 imageVector = FilterList,
@@ -467,5 +471,27 @@ private fun <T : Enum<T>> FilterOptions(
           )
         }
     }
+  }
+}
+
+/** A very simple alternative to `IconButton` that supports [onLongClick] too. */
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun CompositeIconButton(
+  onClick: () -> Unit,
+  onLongClick: () -> Unit,
+  modifier: Modifier = Modifier,
+  content: @Composable () -> Unit,
+) {
+  Box(
+    modifier =
+      modifier
+        .minimumInteractiveComponentSize()
+        .size(40.dp)
+        .clip(MaterialTheme.shapes.small)
+        .combinedClickable(onClick = onClick, onLongClick = onLongClick, role = Role.Button),
+    contentAlignment = Alignment.Center,
+  ) {
+    content()
   }
 }

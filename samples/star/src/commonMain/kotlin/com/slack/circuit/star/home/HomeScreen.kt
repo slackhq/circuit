ae23ee80e9519f725868b7fd8e3807968c962cb5
Zac Sweers <pandanomic@gmail.com>
2024-01-26 22:23:32 -0500

    Don't use child nav in HomeScreen to avoid breaking this

--- samples/star/src/commonMain/kotlin/com/slack/circuit/star/home/HomeScreen.kt
+++ samples/star/src/commonMain/kotlin/com/slack/circuit/star/home/HomeScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -20,8 +21,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.slack.circuit.codegen.annotations.CircuitInject
 import com.slack.circuit.foundation.CircuitContent
-import com.slack.circuit.foundation.NavEvent
-import com.slack.circuit.foundation.onNavEvent
 import com.slack.circuit.retained.rememberRetained
 import com.slack.circuit.runtime.CircuitUiEvent
 import com.slack.circuit.runtime.CircuitUiState
@@ -29,7 +28,6 @@ import com.slack.circuit.runtime.Navigator
 import com.slack.circuit.runtime.screen.Screen
 import com.slack.circuit.star.common.Platform
 import com.slack.circuit.star.di.AppScope
-import com.slack.circuit.star.home.HomeScreen.Event.ChildNav
 import com.slack.circuit.star.home.HomeScreen.Event.ClickNavItem
 import com.slack.circuit.star.parcel.CommonParcelize
 import com.slack.circuit.star.ui.StarTheme
@@ -42,26 +40,24 @@ data object HomeScreen : Screen {
     val navItems: ImmutableList<BottomNavItem> =
       persistentListOf(BottomNavItem.Adoptables, BottomNavItem.About),
     val selectedIndex: Int = 0,
+    val navigator: Navigator,
     val eventSink: (Event) -> Unit,
   ) : CircuitUiState
 
   sealed interface Event : CircuitUiEvent {
     class ClickNavItem(val index: Int) : Event
-
-    class ChildNav(val navEvent: NavEvent) : Event
   }
 }
 
 @CircuitInject(screen = HomeScreen::class, scope = AppScope::class)
 @Composable
 fun HomePresenter(navigator: Navigator): HomeScreen.State {
-  var selectedIndex by remember { mutableStateOf(0) }
-  return HomeScreen.State(selectedIndex = selectedIndex) { event ->
+  var selectedIndex by remember { mutableIntStateOf(0) }
+  return HomeScreen.State(selectedIndex = selectedIndex, navigator = navigator) { event ->
     when (event) {
       is ClickNavItem -> {
         selectedIndex = event.index
       }
-      is ChildNav -> navigator.onNavEvent(event.navEvent)
     }
   }
 }
@@ -84,11 +80,7 @@ fun HomeContent(state: HomeScreen.State, modifier: Modifier = Modifier) {
   ) { paddingValues ->
     contentComposed = true
     val screen = state.navItems[state.selectedIndex].screen
-    CircuitContent(
-      screen,
-      modifier = Modifier.padding(paddingValues),
-      onNavEvent = { event -> state.eventSink(ChildNav(event)) },
-    )
+    CircuitContent(screen, modifier = Modifier.padding(paddingValues), navigator = state.navigator)
   }
   Platform.ReportDrawnWhen { contentComposed }
 }

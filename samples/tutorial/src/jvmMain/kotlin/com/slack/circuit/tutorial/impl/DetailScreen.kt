2fd3fb8550c267c502aeafa115f92964aba75e82
Zac Sweers <pandanomic@gmail.com>
2024-02-01 18:25:09 -0500

    Spotless

--- samples/tutorial/src/jvmMain/kotlin/com/slack/circuit/tutorial/impl/DetailScreen.kt
+++ samples/tutorial/src/jvmMain/kotlin/com/slack/circuit/tutorial/impl/DetailScreen.kt
@@ -13,9 +13,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.slack.circuit.runtime.CircuitContext

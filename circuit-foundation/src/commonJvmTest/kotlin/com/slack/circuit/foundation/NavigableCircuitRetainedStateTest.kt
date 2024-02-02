b589fd0505bac3a8dd45945b00126e7a5f4e6a1a
Zac Sweers <pandanomic@gmail.com>
2024-02-02 16:09:05 -0500

    Fixes

--- circuit-foundation/src/commonJvmTest/kotlin/com/slack/circuit/foundation/NavigableCircuitRetainedStateTest.kt
+++ circuit-foundation/src/commonJvmTest/kotlin/com/slack/circuit/foundation/NavigableCircuitRetainedStateTest.kt
@@ -115,13 +115,13 @@ class NavigableCircuitRetainedStateTest {
 
       setContent {
         CircuitCompositionLocals(circuit) {
-          val backstack = rememberSaveableBackStack { push(TestScreen.RootAlpha) }
+          val backStack = rememberSaveableBackStack { push(TestScreen.RootAlpha) }
           val navigator =
             rememberCircuitNavigator(
-              backstack = backstack,
+              backStack = backStack,
               onRootPop = {}, // no-op for tests
             )
-          NavigableCircuitContent(navigator = navigator, backstack = backstack)
+          NavigableCircuitContent(navigator = navigator, backStack = backStack)
         }
       }
 

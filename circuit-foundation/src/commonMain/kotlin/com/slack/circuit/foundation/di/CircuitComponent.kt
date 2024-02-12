91b79218ad1173035eabbe95a7f100d52ab749c6
Zac Sweers <pandanomic@gmail.com>
2024-02-12 02:18:58 -0500

    Add CircuitComponent

---
+++ circuit-foundation/src/commonMain/kotlin/com/slack/circuit/foundation/di/CircuitComponent.kt
@@ -0,0 +1,22 @@
+package com.slack.circuit.foundation.di
+
+import com.slack.circuit.foundation.Circuit
+import com.slack.circuit.runtime.presenter.Presenter
+import com.slack.circuit.runtime.ui.Ui
+
+/**
+ * A simple interface that exposes [uiFactories] and [presenterFactories]. This can be useful for DI
+ * frameworks that expose multibindings for factories.
+ */
+public interface CircuitComponent {
+  public val uiFactories: Set<Ui.Factory>
+  public val presenterFactories: Set<Presenter.Factory>
+}
+
+/**
+ * Returns a new [Circuit.Builder] initialized with this [CircuitComponent.uiFactories] and
+ * [CircuitComponent.presenterFactories] added.
+ */
+public fun CircuitComponent.circuitBuilder(): Circuit.Builder {
+  return Circuit.Builder().addPresenterFactories(presenterFactories).addUiFactories(uiFactories)
+}

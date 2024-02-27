9e55d63178e95292a3cb8384160d96dcc79a725d
Zac Sweers <pandanomic@gmail.com>
2024-02-27 15:26:44 -0500

    Add back missing locals

--- samples/counter/apps/src/jsMain/kotlin/com/slack/circuit/sample/counter/browser/main.kt
+++ samples/counter/apps/src/jsMain/kotlin/com/slack/circuit/sample/counter/browser/main.kt
@@ -1,7 +1,22 @@
 // Copyright (C) 2023 Slack Technologies, LLC
 // SPDX-License-Identifier: Apache-2.0
+@file:Suppress("DEPRECATION_ERROR")
+
 package com.slack.circuit.sample.counter.browser
 
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.input.InputMode
+import androidx.compose.ui.input.InputModeManager
+import androidx.compose.ui.platform.DefaultViewConfiguration
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalFontFamilyResolver
+import androidx.compose.ui.platform.LocalInputModeManager
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.platform.LocalViewConfiguration
+import androidx.compose.ui.text.font.createFontFamilyResolver
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
 import com.slack.circuit.foundation.Circuit
 import com.slack.circuit.foundation.CircuitCompositionLocals
 import com.slack.circuit.foundation.CircuitContent
@@ -18,7 +33,23 @@ fun main() {
       .addPresenterFactory(CounterPresenterFactory())
       .addUiFactory(CounterUiFactory())
       .build()
+  // https://github.com/JetBrains/compose-multiplatform/issues/2186
+  val fontFamilyResolver = createFontFamilyResolver()
   renderComposable(rootElementId = "root") {
-    CircuitCompositionLocals(circuit) { CircuitContent(BrowserCounterScreen) }
+    CompositionLocalProvider(
+      LocalDensity provides Density(1.0f),
+      LocalLayoutDirection provides LayoutDirection.Ltr,
+      LocalViewConfiguration provides DefaultViewConfiguration(Density(1.0f)),
+      LocalInputModeManager provides InputModeManagerObject,
+      LocalFontFamilyResolver provides fontFamilyResolver,
+    ) {
+      CircuitCompositionLocals(circuit) { CircuitContent(BrowserCounterScreen) }
+    }
   }
+}
+
+private object InputModeManagerObject : InputModeManager {
+  override val inputMode = InputMode.Keyboard
+
+  @ExperimentalComposeUiApi override fun requestInputMode(inputMode: InputMode) = false
 }

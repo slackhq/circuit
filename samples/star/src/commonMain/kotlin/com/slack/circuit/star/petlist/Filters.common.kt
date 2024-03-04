2671d73354f3464f670fac45d28b39cc00295b67
Zac Sweers <pandanomic@gmail.com>
2024-03-03 20:57:23 -0500

    Remove commonized parceler infra
    The immutable collections we used this for are natively supported in K2 beta1

--- samples/star/src/commonMain/kotlin/com/slack/circuit/star/petlist/Filters.common.kt
+++ samples/star/src/commonMain/kotlin/com/slack/circuit/star/petlist/Filters.common.kt
@@ -7,16 +7,12 @@ import com.slack.circuit.star.db.Gender
 import com.slack.circuit.star.db.Size
 import com.slack.circuit.star.parcel.CommonParcelable
 import com.slack.circuit.star.parcel.CommonParcelize
-import com.slack.circuit.star.parcel.CommonTypeParceler
-import com.slack.circuit.star.parcel.ImmutableSetParceler
 import kotlinx.collections.immutable.ImmutableSet
 import kotlinx.collections.immutable.toImmutableSet
 
 @CommonParcelize
 class Filters(
-  @CommonTypeParceler<ImmutableSet<Gender>, ImmutableSetParceler>
   val genders: ImmutableSet<Gender> = Gender.entries.asIterable().toImmutableSet(),
-  @CommonTypeParceler<ImmutableSet<Size>, ImmutableSetParceler>
   val sizes: ImmutableSet<Size> = Size.entries.asIterable().toImmutableSet(),
 ) : CommonParcelable
 

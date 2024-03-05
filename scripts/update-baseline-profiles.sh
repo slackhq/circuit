df661ec95f88742c7e5962976a2bb979cbc71b29
Zac Sweers <pandanomic@gmail.com>
2024-03-05 17:33:13 -0500

    CC is broken

--- scripts/update-baseline-profiles.sh
+++ scripts/update-baseline-profiles.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -uo pipefail
 
-./gradlew cleanManagedDevices && ./gradlew generateBaselineProfile
+# TODO use CC when it's fixed https://issuetracker.google.com/issues/328117812
+./gradlew cleanManagedDevices && ./gradlew generateBaselineProfile --no-configuration-cache

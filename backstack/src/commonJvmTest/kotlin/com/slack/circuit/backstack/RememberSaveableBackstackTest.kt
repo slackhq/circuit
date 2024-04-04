cd5dfe1d8653d012ea5272a1b5fa6d48f89b79a8
Justin Bisignano <jbisignano@slack-corp.com>
2024-04-04 17:21:11 -0600

    Run formatter

--- backstack/src/commonJvmTest/kotlin/com/slack/circuit/backstack/RememberSaveableBackstackTest.kt
+++ backstack/src/commonJvmTest/kotlin/com/slack/circuit/backstack/RememberSaveableBackstackTest.kt
@@ -8,69 +8,67 @@ import app.cash.molecule.moleculeFlow
 import app.cash.turbine.test
 import com.slack.circuit.internal.test.TestScreen
 import com.slack.circuit.runtime.screen.Screen
-import kotlinx.coroutines.test.runTest
-import org.junit.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNotSame
 import kotlin.test.assertSame
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
 
 class RememberSaveableBackstackTest {
 
-    @Test
-    fun backStackStartsWithRootScreen() = runTest {
-        moleculeFlow(RecompositionMode.Immediate) {
-            val backStack = rememberSaveableBackStack(TestScreen.ScreenA)
-            backStack.entryList.toList()
-        }.test {
-            assertEquals(awaitItem().first().screen, TestScreen.ScreenA)
-        }
-    }
+  @Test
+  fun backStackStartsWithRootScreen() = runTest {
+    moleculeFlow(RecompositionMode.Immediate) {
+        val backStack = rememberSaveableBackStack(TestScreen.ScreenA)
+        backStack.entryList.toList()
+      }
+      .test { assertEquals(awaitItem().first().screen, TestScreen.ScreenA) }
+  }
 
-    @Test
-    fun rememberSaveableBackStackReturnsSameInstanceWithSameParams() = runTest {
-        val rootScreen by mutableStateOf<Screen>(TestScreen.ScreenA)
-        var dummyData by mutableStateOf(false)
-        moleculeFlow(RecompositionMode.Immediate) {
-            dummyData
-            rememberSaveableBackStack(rootScreen)
-        }.test {
-            val firstStack = awaitItem()
-            dummyData = true // Force a recomposition
-            val secondStack = awaitItem()
+  @Test
+  fun rememberSaveableBackStackReturnsSameInstanceWithSameParams() = runTest {
+    val rootScreen by mutableStateOf<Screen>(TestScreen.ScreenA)
+    var dummyData by mutableStateOf(false)
+    moleculeFlow(RecompositionMode.Immediate) {
+        dummyData
+        rememberSaveableBackStack(rootScreen)
+      }
+      .test {
+        val firstStack = awaitItem()
+        dummyData = true // Force a recomposition
+        val secondStack = awaitItem()
 
-            assertSame(firstStack, secondStack)
-        }
-    }
+        assertSame(firstStack, secondStack)
+      }
+  }
 
-    @Test
-    fun rememberSaveableBackStackReturnsNewInstanceWithChangedRoot() = runTest {
-        var rootScreen by mutableStateOf<Screen>(TestScreen.ScreenA)
-        moleculeFlow(RecompositionMode.Immediate) {
-            rememberSaveableBackStack(rootScreen)
-        }.test {
-            val firstStack = awaitItem()
-            rootScreen = TestScreen.ScreenB
-            val secondStack = awaitItem()
+  @Test
+  fun rememberSaveableBackStackReturnsNewInstanceWithChangedRoot() = runTest {
+    var rootScreen by mutableStateOf<Screen>(TestScreen.ScreenA)
+    moleculeFlow(RecompositionMode.Immediate) { rememberSaveableBackStack(rootScreen) }
+      .test {
+        val firstStack = awaitItem()
+        rootScreen = TestScreen.ScreenB
+        val secondStack = awaitItem()
 
-            assertNotSame(firstStack, secondStack)
-            assertEquals(firstStack.entryList.first().screen, TestScreen.ScreenA)
-            assertEquals(secondStack.entryList.first().screen, TestScreen.ScreenB)
-        }
-    }
+        assertNotSame(firstStack, secondStack)
+        assertEquals(firstStack.entryList.first().screen, TestScreen.ScreenA)
+        assertEquals(secondStack.entryList.first().screen, TestScreen.ScreenB)
+      }
+  }
 
-    @Test
-    fun rememberSaveableBackStackStartedWithListReturnsNewInstanceWithChangedRoot() = runTest {
-        var rootScreens by mutableStateOf<List<Screen>>(listOf(TestScreen.ScreenA))
-        moleculeFlow(RecompositionMode.Immediate) {
-            rememberSaveableBackStack(rootScreens)
-        }.test {
-            val firstStack = awaitItem()
-            rootScreens = listOf(TestScreen.ScreenB)
-            val secondStack = awaitItem()
+  @Test
+  fun rememberSaveableBackStackStartedWithListReturnsNewInstanceWithChangedRoot() = runTest {
+    var rootScreens by mutableStateOf<List<Screen>>(listOf(TestScreen.ScreenA))
+    moleculeFlow(RecompositionMode.Immediate) { rememberSaveableBackStack(rootScreens) }
+      .test {
+        val firstStack = awaitItem()
+        rootScreens = listOf(TestScreen.ScreenB)
+        val secondStack = awaitItem()
 
-            assertNotSame(firstStack, secondStack)
-            assertEquals(firstStack.entryList.first().screen, TestScreen.ScreenA)
-            assertEquals(secondStack.entryList.first().screen, TestScreen.ScreenB)
-        }
-    }
+        assertNotSame(firstStack, secondStack)
+        assertEquals(firstStack.entryList.first().screen, TestScreen.ScreenA)
+        assertEquals(secondStack.entryList.first().screen, TestScreen.ScreenB)
+      }
+  }
 }

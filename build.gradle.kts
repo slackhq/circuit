8ebb8907838f1c8217dee1b2f90fb42ee3cf3fd9
Emmanuel Boudrant <emmanuel.boudrant@gmail.com>
2024-04-30 13:51:42 -0700

    Target Java 1.8 for Circuit published libraries (from Java 11) (#1378)
    Our problem is we want to use Circuit in a SDK potentially used by AGP
    4.0 / Java 8 consumers (Unity). Since Circuit target Java 11 we can't
    build the consumer app. Since current androidx libraries are built for
    Java 8 it makes sense to downgrade from Java 11 to Java 8 (until
    androidx team decide to bump to Java 11).
    
    In this change only published libraries are impacted. Published library
    mean a `POM_ARTIFACT_ID` property exists.
    
    The change was tested successfully using a Unity 2020 project with the
    Circuit counter sample added to our SDK.

--- build.gradle.kts
+++ build.gradle.kts
@@ -139,8 +139,12 @@ fun Project.configureComposeBom(dependencyHandler: DependencyHandler) {
 }
 
 val jvmTargetVersion = libs.versions.jvmTarget
+val publishedJvmTargetVersion = libs.versions.publishedJvmTarget
 
 subprojects {
+  val isPublished = project.hasProperty("POM_ARTIFACT_ID")
+  val jvmTargetProject = if (isPublished) publishedJvmTargetVersion else jvmTargetVersion
+
   pluginManager.withPlugin("java") {
     configure<JavaPluginExtension> {
       toolchain {
@@ -151,7 +155,9 @@ subprojects {
     }
 
     tasks.withType<JavaCompile>().configureEach {
-      options.release.set(jvmTargetVersion.map(String::toInt))
+      options.release.set(
+        jvmTargetProject.map(JavaVersion::toVersion).map { it.majorVersion.toInt() }
+      )
     }
 
     // This is the default base plugin applied on all projects, so safe to add this hook here
@@ -167,7 +173,12 @@ subprojects {
       compilerOptions {
         allWarningsAsErrors.set(true)
         if (this is KotlinJvmCompilerOptions) {
-          jvmTarget.set(jvmTargetVersion.map(JvmTarget::fromTarget))
+          jvmTarget.set(
+            jvmTargetProject
+              .map(JavaVersion::toVersion)
+              .map { it.toString() }
+              .map(JvmTarget::fromTarget)
+          )
           // Stub gen copies args from the parent compilation
           if (this@configureEach !is KaptGenerateStubsTask) {
             freeCompilerArgs.addAll(
@@ -340,8 +351,8 @@ subprojects {
     }
 
     compileOptions {
-      sourceCompatibility = JavaVersion.VERSION_11
-      targetCompatibility = JavaVersion.VERSION_11
+      sourceCompatibility = jvmTargetProject.map(JavaVersion::toVersion).get()
+      targetCompatibility = jvmTargetProject.map(JavaVersion::toVersion).get()
     }
 
     lint {

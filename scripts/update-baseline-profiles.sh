569a7721409281ef8632159e61d77869ef4cbed8
Zac Sweers <pandanomic@gmail.com>
2024-05-13 14:25:36 -0400

    Updates

--- scripts/update-baseline-profiles.sh
+++ scripts/update-baseline-profiles.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -uo pipefail
 
-# TODO use CC when it's fixed https://issuetracker.google.com/issues/328117812
-./gradlew cleanManagedDevices && ./gradlew generateBaselineProfile --no-configuration-cache
+./gradlew cleanManagedDevices --unused-only
+./gradlew generateBaselineProfile

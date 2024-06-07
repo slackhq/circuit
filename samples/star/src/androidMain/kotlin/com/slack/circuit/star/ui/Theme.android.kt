c7a29cdbe2828d3df2123e1d91ff0b4910db4415
Zac Sweers <pandanomic@gmail.com>
2024-06-06 21:04:49 -0400

    Fix status bar color

--- samples/star/src/androidMain/kotlin/com/slack/circuit/star/ui/Theme.android.kt
+++ samples/star/src/androidMain/kotlin/com/slack/circuit/star/ui/Theme.android.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -34,7 +35,7 @@ actual fun StarTheme(
   if (!view.isInEditMode) {
     SideEffect {
       val window = (view.context as Activity).window
-      window.statusBarColor = colorScheme.primary.toArgb()
+      window.statusBarColor = Color.Transparent.toArgb()
       WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = useDarkTheme
     }
   }

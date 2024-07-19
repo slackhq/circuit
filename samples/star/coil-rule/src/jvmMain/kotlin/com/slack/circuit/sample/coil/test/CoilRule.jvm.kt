b37e0c251205f3bd7433b09da7525c490f04151d
Zac Sweers <pandanomic@gmail.com>
2024-07-18 23:31:33 -0400

    Fix another

--- samples/star/coil-rule/src/jvmMain/kotlin/com/slack/circuit/sample/coil/test/CoilRule.jvm.kt
+++ samples/star/coil-rule/src/jvmMain/kotlin/com/slack/circuit/sample/coil/test/CoilRule.jvm.kt
@@ -3,11 +3,11 @@
 package com.slack.circuit.sample.coil.test
 
 import coil3.Image
-import coil3.asCoilImage
+import coil3.asImage
 import org.jetbrains.skia.Bitmap
 import org.jetbrains.skia.Image as SkiaImage
 
 actual fun createImageFromBytes(bytes: ByteArray): Image {
   val skiaImage = SkiaImage.makeFromEncoded(bytes)
-  return Bitmap.makeFromImage(skiaImage).asCoilImage()
+  return Bitmap.makeFromImage(skiaImage).asImage()
 }

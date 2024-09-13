4f6066b958e50f55a314c00dd91aa70b5c4992aa
Zac Sweers <pandanomic@gmail.com>
2024-09-13 15:09:00 -0400

    KI stable + internal API

--- circuit-codegen-annotations/src/commonMain/kotlin/com/slack/circuit/codegen/annotations/ComSlackCircuitCodegenAnnotationsCircuitInject.kt
+++ circuit-codegen-annotations/src/commonMain/kotlin/com/slack/circuit/codegen/annotations/ComSlackCircuitCodegenAnnotationsCircuitInject.kt
@@ -1,5 +1,7 @@
 // Copyright (C) 2024 Slack Technologies, LLC
 // SPDX-License-Identifier: Apache-2.0
+@file:Suppress("PackageDirectoryMismatch")
+
 package amazon.lastmile.inject
 
 import com.slack.circuit.codegen.annotations.CircuitInject
@@ -8,5 +10,5 @@ import software.amazon.lastmile.kotlin.inject.anvil.`internal`.Origin
 
 // TODO temporary until https://github.com/amzn/kotlin-inject-anvil/issues/24
 @Origin(value = CircuitInject::class)
-public val comSlackCircuitCodegenAnnotationsCircuitInject: KClass<CircuitInject> =
+internal val comSlackCircuitCodegenAnnotationsCircuitInject: KClass<CircuitInject> =
   CircuitInject::class

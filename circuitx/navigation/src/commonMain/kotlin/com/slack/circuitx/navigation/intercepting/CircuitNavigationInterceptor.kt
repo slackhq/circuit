8c15664d929662554c8795d57ab6f2c6be674011
Josh Stagg <jstagg@slack-corp.com>
2024-09-19 16:32:54 -0700

    CircuitNavigationInterceptor

---
+++ circuitx/navigation/src/commonMain/kotlin/com/slack/circuitx/navigation/intercepting/CircuitNavigationInterceptor.kt
@@ -0,0 +1,61 @@
+package com.slack.circuitx.navigation.intercepting
+
+import com.slack.circuit.runtime.screen.PopResult
+import com.slack.circuit.runtime.screen.Screen
+import kotlinx.collections.immutable.ImmutableList
+
+/**
+ * A custom navigation processor for use with [CircuitInterceptingNavigator].
+ *
+ * @see CircuitInterceptingNavigator
+ */
+public interface CircuitNavigationInterceptor {
+
+  /**
+   * Navigates to the [screen], returning a [Result] for the navigation.
+   *
+   * By default this will skip the navigation and return [Skipped].
+   */
+  public fun goTo(screen: Screen): Result = Skipped
+
+  /**
+   * Navigates back looking at the [peekBackStack], returning a [Result] for the navigation.
+   *
+   * By default this will skip the navigation and return [Skipped].
+   */
+  public fun pop(peekBackStack: ImmutableList<Screen>, result: PopResult?): Result = Skipped
+
+  /** The result of the [CircuitNavigationInterceptor] intercepting [goTo] or [pop]. */
+  public sealed interface Result {
+
+    /** The [CircuitNavigationInterceptor] did not intercept the interaction. */
+    public data object Skipped : Result
+
+    /**
+     * The [CircuitNavigationInterceptor] interaction was successful.
+     *
+     * @param consumed If the [CircuitNavigationInterceptor] consumed the interaction.
+     */
+    public data class Success(val consumed: Boolean) : Result
+
+    /**
+     * The [CircuitNavigationInterceptor] interaction was unsuccessful.
+     *
+     * @param consumed If the [CircuitNavigationInterceptor] consumed the interaction.
+     */
+    public data class Failure(val consumed: Boolean, val reason: Throwable? = null) : Result
+  }
+
+  public companion object {
+    /**
+     * Shorthand for a [Result.Skipped] interceptor result where the interceptor did not intercept
+     * the navigation.
+     */
+    public val Skipped: Result = Result.Skipped
+    /**
+     * Shorthand for a [Result.Success] interceptor result where the interceptor has consumed the
+     * navigation.
+     */
+    public val ConsumedSuccess: Result = Result.Success(consumed = true)
+  }
+}

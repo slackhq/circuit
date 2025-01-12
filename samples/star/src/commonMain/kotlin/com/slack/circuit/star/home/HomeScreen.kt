9714ce3b4ee900179c570f6ba94dfbe4ba01c79c
vulpeszerda <teksu.lee@gmail.com>
2025-01-12 19:40:09 +0900

    Added RetainedStateHolder to star sample

--- samples/star/src/commonMain/kotlin/com/slack/circuit/star/home/HomeScreen.kt
+++ samples/star/src/commonMain/kotlin/com/slack/circuit/star/home/HomeScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.saveable.rememberSaveableStateHolder
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,9 +29,11 @@ import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.unit.IntOffset
 import com.slack.circuit.codegen.annotations.CircuitInject
 import com.slack.circuit.foundation.CircuitContent
+import com.slack.circuit.foundation.LocalCircuit
 import com.slack.circuit.foundation.NavEvent
 import com.slack.circuit.foundation.onNavEvent
 import com.slack.circuit.retained.rememberRetained
+import com.slack.circuit.retained.rememberRetainedStateHolder
 import com.slack.circuit.runtime.CircuitUiEvent
 import com.slack.circuit.runtime.CircuitUiState
 import com.slack.circuit.runtime.Navigator
@@ -127,12 +130,19 @@ fun HomeContent(state: HomeScreen.State, modifier: Modifier = Modifier) =
       },
     ) { paddingValues ->
       contentComposed = true
-      val screen = state.navItems[state.selectedIndex].screen
-      CircuitContent(
-        screen,
-        modifier = Modifier.padding(paddingValues),
-        onNavEvent = { event -> state.eventSink(ChildNav(event)) },
-      )
+      val saveableStateHolder = rememberSaveableStateHolder()
+      val retainedStateHolder = rememberRetainedStateHolder()
+      val currentScreen = state.navItems[state.selectedIndex].screen
+      saveableStateHolder.SaveableStateProvider(currentScreen) {
+        retainedStateHolder.RetainedStateProvider(state.selectedIndex.toString()) {
+          CircuitContent(
+            currentScreen,
+            modifier = Modifier.padding(paddingValues),
+            onNavEvent = { event -> state.eventSink(ChildNav(event)) },
+          )
+        }
+      }
+      contentComposed = true
     }
     Platform.ReportDrawnWhen { contentComposed }
   }

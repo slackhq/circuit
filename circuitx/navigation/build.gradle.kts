ae280200ce2fca320dda8abed3e79de9bf319363
Josh Stagg <jstagg@slack-corp.com>
2025-06-09 09:03:21 -0700

    Better `BackHandler` guarding of root pops (#2131)
    - Only fire the root pop in the navigators `BackHandler` once, and then
    reset the trigger if the root screen changes

--- circuitx/navigation/build.gradle.kts
+++ circuitx/navigation/build.gradle.kts
@@ -42,6 +42,7 @@ kotlin {
     }
     commonTest {
       dependencies {
+        implementation(libs.androidx.lifecycle.runtime.compose.jb)
         implementation(libs.coroutines.test)
         implementation(libs.kotlin.test)
         implementation(libs.molecule.runtime)

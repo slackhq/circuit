0fa6b46b4b5c42984517465dfe674bb05ac955a8
Zac Sweers <pandanomic@gmail.com>
2025-07-08 15:04:34 -0400

    iOS graph

---
+++ samples/star/src/iosMain/kotlin/com/slack/circuit/star/di/BaseUiModule.kt
@@ -0,0 +1,19 @@
+// Copyright (C) 2025 Slack Technologies, LLC
+// SPDX-License-Identifier: Apache-2.0
+package com.slack.circuit.star.di
+
+import coil3.PlatformContext
+import dev.zacsweers.metro.AppScope
+import dev.zacsweers.metro.ContributesTo
+import dev.zacsweers.metro.Provides
+import dev.zacsweers.metro.SingleIn
+
+@ContributesTo(AppScope::class)
+interface BaseUiModule {
+  companion object {
+    @ApplicationContext
+    @SingleIn(AppScope::class)
+    @Provides
+    fun provideAppContext(): PlatformContext = PlatformContext.INSTANCE
+  }
+}

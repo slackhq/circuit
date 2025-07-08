038ac5145e878f686b22e327ca9ac8d70aacd026
Zac Sweers <pandanomic@gmail.com>
2025-07-08 15:12:51 -0400

    iOS ConditionalSystemUiColors.ios.kt

---
+++ samples/star/src/iosMain/kotlin/com/slack/circuit/star/ui/ConditionalSystemUiColors.ios.kt
@@ -0,0 +1,35 @@
+package com.slack.circuit.star.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import platform.UIKit.UIApplication
+import platform.UIKit.setStatusBarStyle
+
+private class ConditionalSystemUiColorsImpl(private val application: UIApplication) :
+  ConditionalSystemUiColors {
+  private var storedStatusBarStyle by mutableLongStateOf(application.statusBarStyle)
+
+  override fun save() {
+    storedStatusBarStyle = application.statusBarStyle
+  }
+
+  override fun restore() {
+    application.setStatusBarStyle(storedStatusBarStyle, animated = true)
+  }
+}
+
+@Composable
+actual fun rememberConditionalSystemUiColors(): ConditionalSystemUiColors {
+  val application = UIApplication.sharedApplication
+
+  return remember {
+    if (application.statusBarHidden) {
+      ConditionalSystemUiColors.None
+    } else {
+      ConditionalSystemUiColorsImpl(application = application)
+    }
+  }
+}

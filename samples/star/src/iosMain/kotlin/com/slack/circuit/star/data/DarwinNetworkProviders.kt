4a3a5954db6031c89326af7e72cd784a0627a6ed
Zac Sweers <pandanomic@gmail.com>
2025-07-08 19:44:17 -0400

    Try to match ios cache to jvm

--- samples/star/src/iosMain/kotlin/com/slack/circuit/star/data/DarwinNetworkProviders.kt
+++ samples/star/src/iosMain/kotlin/com/slack/circuit/star/data/DarwinNetworkProviders.kt
@@ -6,12 +6,41 @@ import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.ContributesTo
 import dev.zacsweers.metro.Provides
 import dev.zacsweers.metro.SingleIn
-import io.ktor.client.engine.*
-import io.ktor.client.engine.darwin.*
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.engine.HttpClientEngineFactory
+import io.ktor.client.engine.darwin.Darwin
+import io.ktor.client.engine.darwin.DarwinClientEngineConfig
+import platform.Foundation.NSURLCache
+import platform.Foundation.NSURLSessionConfiguration
 
 @ContributesTo(AppScope::class)
-interface DarwinNetworkProviders {
+interface IosNetworkProviders {
   @Provides
   @SingleIn(AppScope::class)
-  fun provideHttpClientEngineFactory(): HttpClientEngineFactory<*> = Darwin
+  fun provideHttpClientEngineFactory(
+    appDirs: StarAppDirs,
+    configBlock: DarwinClientEngineConfig.() -> Unit,
+  ): HttpClientEngineFactory<*> =
+    object : HttpClientEngineFactory<DarwinClientEngineConfig> {
+      override fun create(block: DarwinClientEngineConfig.() -> Unit): HttpClientEngine {
+        return Darwin.create {
+          configureSession {
+            val configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
+
+            // Configure NSURLCache similar to OkHttp cache
+            val cacheDirectory = CommonDataProviders.httpCacheDir(appDirs)
+            appDirs.fs.createDirectories(cacheDirectory)
+
+            val cache =
+              NSURLCache(
+                memoryCapacity = CommonDataProviders.MEMORY_CACHE_SIZE.toULong(),
+                diskCapacity = CommonDataProviders.MAX_CACHE_SIZE.toULong(),
+                diskPath = cacheDirectory.toString(),
+              )
+            configuration.setURLCache(cache)
+          }
+          block()
+        }
+      }
+    }
 }

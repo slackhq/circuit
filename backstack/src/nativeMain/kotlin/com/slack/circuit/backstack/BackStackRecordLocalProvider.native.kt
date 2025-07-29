309a401d6b8f2b7c979e914ceb0fd6942cef5116
Josh Stagg <jstagg@slack-corp.com>
2025-07-28 18:53:56 -0700

    Rework `ViewModelBackStackRecordLocalProvider` (#2207)
    # Changes
    - Change to use the compose multi-platform view model
    - Moved `ViewModelBackStackRecordLocalProvider` into circuit foundation
    out of `backstack`
        - Think it makes more sense for this to exist within foundation?
    - Allows for using circuit retained instead of fishing for an Activity
    - Provide the `ViewModelBackStackRecordLocalProvider` as default on the
    circuit instance instead of as a static val
        - Now allows for setting default `BackStackRecordLocalProvider`'s
    - Add `backStackHostViewModel()` to allow access to a `ViewModel` in the
    host of the `NavigableCircuitContent`

--- backstack/src/nativeMain/kotlin/com/slack/circuit/backstack/BackStackRecordLocalProvider.native.kt
+++
@@ -1,9 +0,0 @@
-package com.slack.circuit.backstack
-
-/**
- * This is specifically a get() rather than a statically initialized property. The Kotlin/Native
- * optimizer seems to trip up otherwise: https://github.com/slackhq/circuit/issues/1075
- */
-internal actual val defaultBackStackRecordLocalProviders:
-  List<BackStackRecordLocalProvider<BackStack.Record>>
-  get() = emptyList()

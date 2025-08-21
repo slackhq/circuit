64a604b4a140f6b348145da473bdd2fbe345590e
Zac Sweers <pandanomic@gmail.com>
2025-08-21 19:57:17 -0400

    Remove from Screen

--- circuit-runtime-screen/src/androidMain/kotlin/com/slack/circuit/runtime/screen/Screen.android.kt
+++ circuit-runtime-screen/src/androidMain/kotlin/com/slack/circuit/runtime/screen/Screen.android.kt
@@ -2,9 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 package com.slack.circuit.runtime.screen
 
-import android.os.Parcelable
 import androidx.compose.runtime.Immutable
 
-@Immutable public actual interface Screen : Parcelable
+@Immutable public actual interface Screen
 
 @Immutable public actual interface StaticScreen : Screen

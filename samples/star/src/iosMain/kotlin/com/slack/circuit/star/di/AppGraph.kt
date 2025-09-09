5643586245dc529942426ec7f767dec37612ce06
Zac Sweers <pandanomic@gmail.com>
2025-09-09 17:44:46 +0900

    Split up ios app graphs

--- samples/star/src/iosMain/kotlin/com/slack/circuit/star/di/AppGraph.kt
+++ samples/star/src/iosMain/kotlin/com/slack/circuit/star/di/AppGraph.kt
@@ -2,14 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 package com.slack.circuit.star.di
 
-import dev.zacsweers.metro.AppScope
-import dev.zacsweers.metro.DependencyGraph
-import dev.zacsweers.metro.createGraph
-
-@DependencyGraph(scope = AppScope::class)
-interface AppGraph : CommonAppGraph {
-
+// expect/actual due to https://github.com/google/ksp/issues/929
+expect interface AppGraph : CommonAppGraph {
   companion object {
-    fun create(): AppGraph = createGraph<AppGraph>()
+    fun create(): AppGraph
   }
 }

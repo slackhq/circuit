d8d0d4e0598c2f36dec96a26e8027efc9b453555
Zac Sweers <pandanomic@gmail.com>
2025-12-30 02:33:53 -0500

    Spotless

--- samples/star/src/commonMain/kotlin/com/slack/circuit/star/data/socialteesnyc/models.kt
+++ samples/star/src/commonMain/kotlin/com/slack/circuit/star/data/socialteesnyc/models.kt
@@ -52,8 +52,7 @@ data class Animal(
   val descriptionHtml: String? = null,
   /** Markdown description (converted from HTML) */
   val descriptionMarkdown: String? = null,
-  @SerialName("short_description")
-  val shortDescription: String? = null,
+  @SerialName("short_description") val shortDescription: String? = null,
   /** Physical color */
   val color: String? = null,
   /** Compatibility and status attributes */

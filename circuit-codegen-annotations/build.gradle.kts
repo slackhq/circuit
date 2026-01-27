9551f8f0be0a18dadef79db4158870ed711e26b5
Zac Sweers <pandanomic@gmail.com>
2026-01-27 15:33:15 -0500

    Update to KIA 0.1.7 + enable windows target (#2530)
    ~Now up to KIA~

--- circuit-codegen-annotations/build.gradle.kts
+++ circuit-codegen-annotations/build.gradle.kts
@@ -28,8 +28,7 @@ kotlin {
   macosArm64()
   linuxArm64()
   linuxX64()
-  // TODO https://github.com/evant/kotlin-inject/pull/440
-  //  mingwX64()
+  mingwX64()
   js(IR) {
     outputModuleName = property("POM_ARTIFACT_ID").toString()
     browser()

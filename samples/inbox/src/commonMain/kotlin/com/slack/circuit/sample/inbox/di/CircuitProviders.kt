c3016b04a1491e489865a663e0ae167e3a5214b2
Zac Sweers <pandanomic@gmail.com>
2026-05-29 11:35:47 +0200

    Basic DI

---
+++ samples/inbox/src/commonMain/kotlin/com/slack/circuit/sample/inbox/di/CircuitProviders.kt
@@ -0,0 +1,30 @@
+// Copyright (C) 2026 Slack Technologies, LLC
+// SPDX-License-Identifier: Apache-2.0
+package com.slack.circuit.sample.inbox.di
+
+import com.slack.circuit.foundation.Circuit
+import com.slack.circuit.runtime.presenter.Presenter
+import com.slack.circuit.runtime.ui.Ui
+import dev.zacsweers.metro.AppScope
+import dev.zacsweers.metro.ContributesTo
+import dev.zacsweers.metro.Multibinds
+import dev.zacsweers.metro.Provides
+import dev.zacsweers.metro.SingleIn
+
+@ContributesTo(AppScope::class)
+interface CircuitProviders {
+  @Multibinds fun presenterFactories(): Set<Presenter.Factory>
+
+  @Multibinds fun uiFactories(): Set<Ui.Factory>
+
+  @SingleIn(AppScope::class)
+  @Provides
+  fun provideCircuit(
+    presenterFactories: Set<Presenter.Factory>,
+    uiFactories: Set<Ui.Factory>,
+  ): Circuit =
+    Circuit.Builder()
+      .addPresenterFactories(presenterFactories)
+      .addUiFactories(uiFactories)
+      .build()
+}

0dabd48cf2726eadd2eb480c71f2844e3677976f
Zac Sweers <pandanomic@gmail.com>
2026-07-14 22:43:47 -0400

    Compile

--- build-logic/src/main/kotlin/circuit.android.gradle.kts
+++ build-logic/src/main/kotlin/circuit.android.gradle.kts
@@ -37,7 +37,7 @@ pluginManager.withPlugin("com.android.kotlin.multiplatform.library") {
   }
   // todo Robolectric: targetSdk on host test compilations is not getting set on the manifest??
   tasks.matching { it.name == "processAndroidHostTestManifest" }.configureEach {
-    val sdkVersion = targetSdkVersion
+    val sdkVersion = compileSdkVersion
     doLast {
       val manifestDir = outputs.files.files.first { it.isDirectory }
       val manifest = File(manifestDir, "AndroidManifest.xml")

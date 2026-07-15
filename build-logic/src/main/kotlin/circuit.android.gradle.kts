da155960e72738baff9993681cbe744ffca0dda3
Zac Sweers <pandanomic@gmail.com>
2026-07-14 22:45:23 -0400

    Format

--- build-logic/src/main/kotlin/circuit.android.gradle.kts
+++ build-logic/src/main/kotlin/circuit.android.gradle.kts
@@ -36,21 +36,25 @@ pluginManager.withPlugin("com.android.kotlin.multiplatform.library") {
       }
   }
   // todo Robolectric: targetSdk on host test compilations is not getting set on the manifest??
-  tasks.matching { it.name == "processAndroidHostTestManifest" }.configureEach {
-    val sdkVersion = compileSdkVersion
-    doLast {
-      val manifestDir = outputs.files.files.first { it.isDirectory }
-      val manifest = File(manifestDir, "AndroidManifest.xml")
-      if (manifest.exists()) {
-        manifest.writeText(
-          manifest.readText().replace(
-            Regex("""android:targetSdkVersion="\d+""""),
-            """android:targetSdkVersion="$sdkVersion""""
+  tasks
+    .matching { it.name == "processAndroidHostTestManifest" }
+    .configureEach {
+      val sdkVersion = compileSdkVersion
+      doLast {
+        val manifestDir = outputs.files.files.first { it.isDirectory }
+        val manifest = File(manifestDir, "AndroidManifest.xml")
+        if (manifest.exists()) {
+          manifest.writeText(
+            manifest
+              .readText()
+              .replace(
+                Regex("""android:targetSdkVersion="\d+""""),
+                """android:targetSdkVersion="$sdkVersion"""",
+              )
           )
-        )
+        }
       }
     }
-  }
   dependencies { add("lintChecks", catalog.findLibrary("lints-compose").get()) }
 }
 

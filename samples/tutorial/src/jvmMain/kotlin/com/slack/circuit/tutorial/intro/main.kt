a95cf59c7e918cd5080c42bf69eaa005bb89709c
Zac Sweers <pandanomic@gmail.com>
2026-07-21 21:19:15 -0400

    Add serialization to tutorial sample

--- samples/tutorial/src/jvmMain/kotlin/com/slack/circuit/tutorial/intro/main.kt
+++ samples/tutorial/src/jvmMain/kotlin/com/slack/circuit/tutorial/intro/main.kt
@@ -10,21 +10,24 @@ import com.slack.circuit.foundation.Circuit
 import com.slack.circuit.foundation.CircuitCompositionLocals
 import com.slack.circuit.foundation.NavigableCircuitContent
 import com.slack.circuit.foundation.rememberCircuitNavigator
+import com.slack.circuit.tutorial.buildCircuitSaver
 import com.slack.circuit.tutorial.common.EmailRepository
 
 fun main() {
   val emailRepository = EmailRepository()
+  val circuitSaver = buildCircuitSaver()
   val circuit: Circuit =
     Circuit.Builder()
       .addPresenterFactory(DetailPresenter.Factory(emailRepository))
       .addPresenterFactory(InboxPresenter.Factory(emailRepository))
       .addUi<InboxScreen, InboxScreen.State> { state, modifier -> Inbox(state, modifier) }
       .addUi<DetailScreen, DetailScreen.State> { state, modifier -> EmailDetail(state, modifier) }
+      .setCircuitSaver(circuitSaver)
       .build()
   application {
     Window(title = "Tutorial", onCloseRequest = ::exitApplication) {
       MaterialTheme {
-        val backStack = rememberSaveableBackStack(InboxScreen)
+        val backStack = rememberSaveableBackStack(root = InboxScreen, circuitSaver = circuitSaver)
         val navigator = rememberCircuitNavigator(backStack) { exitApplication() }
         CircuitCompositionLocals(circuit) {
           NavigableCircuitContent(navigator = navigator, backStack = backStack)

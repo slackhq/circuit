c1e63989be8591d76c8be3ac1a0b7d811b330a6a
Zac Sweers <pandanomic@gmail.com>
2026-08-12 21:49:01 -0400

    Remove Parcelable supertype from Screen and PopResult

--- circuit-runtime-screen/src/androidMain/kotlin/com/slack/circuit/runtime/screen/CircuitSaver.android.kt
+++ circuit-runtime-screen/src/androidMain/kotlin/com/slack/circuit/runtime/screen/CircuitSaver.android.kt
@@ -2,4 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0
 package com.slack.circuit.runtime.screen
 
-public actual val DefaultCircuitSaver: CircuitSaver = PassThroughCircuitSaver
+import android.os.Parcelable
+
+public actual val DefaultCircuitSaver: CircuitSaver = AndroidDefaultCircuitSaver
+
+private object AndroidDefaultCircuitSaver : CircuitSaver() {
+  override fun save(value: CircuitSaveable): Any? = value.takeIf { it is Parcelable }
+
+  override fun restore(saved: Any): CircuitSaveable? = saved as? CircuitSaveable
+}
--- circuit-runtime-screen/src/commonMain/kotlin/com/slack/circuit/runtime/screen/ParcelableScreen.kt
+++ circuit-runtime-screen/src/commonMain/kotlin/com/slack/circuit/runtime/screen/ParcelableScreen.kt
@@ -7,8 +7,9 @@ import androidx.compose.runtime.Immutable
 /**
  * A [Screen] that is also `android.os.Parcelable` on Android.
  *
- * Use this for common-code screens that persist via Parcelable on android, since common code cannot
- * reference `android.os.Parcelable` directly. On all other platforms this is just a [Screen].
+ * Use this with [DefaultCircuitSaver] to persist a screen through Android saved state. Common code
+ * cannot reference `android.os.Parcelable` directly. On all other platforms this is just a
+ * [Screen].
  *
  * ```
  * @Parcelize // Or your own `@CommonParcelize` annotation
